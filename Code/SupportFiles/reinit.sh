5132ab6270b7f31c1653bb3cfbb93d0aec84920f
John Manferdelli <jlm@CloudProxy1.(none)>
2013-02-21 09:47:26 -0800

    encrypted io

---
+++ Code/SupportFiles/reinit.sh
@@ -0,0 +1,15 @@
+#
+rm fileClient/cert
+rm fileClient/evidence
+rm fileClient/fileKeys
+rm fileClient/privatekey
+rm fileClient/symkey
+rm fileClient/files/file.test.out
+rm fileServer/cert
+rm fileServer/evidence
+rm fileServer/fileKeys
+rm fileServer/privatekey
+rm fileServer/symkey
+rm fileServer/files/file.test
+rm fileClient.log
+rm fileServer.log

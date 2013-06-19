8ea2894a55cc1b7f6302c5d0112fd2b01f771215
Tom Roeder <tmroeder@google.com>
2013-06-19 12:04:22 -0700

    initial commit of files for an independent implementation of CloudProxy

---
+++ Code/CloudProxy/CloudClient.h
@@ -0,0 +1,14 @@
+#ifndef __CLOUD_CLIENT_H_
+#define __CLOUD_CLIENT_H_
+
+namespace cloudproxy {
+class CloudClient {
+// IAH: design the client interface, including protobuf schema for the required
+// data
+
+// messages exchanged by CloudProxy other than TLS:
+// - operations on clouds: 
+};
+}
+
+#endif /* ndef __CLOUD_CLIENT_H_ */

b94536db652a6dbf37b7e48613d0788b637cec7e
Tom Roeder <tmroeder@google.com>
2013-08-29 19:07:28 -0700

    Added a version of the host KVM changes that compiles. This is a modification of Rekha's code to make it compile in the kernel. I don't think this is the right way to do it. But this version compiles.
    This commit also fixes some bugs in the instructions in the README in
    hypercalls and updates that driver to use the Kbuild architecture

---
+++ Code/tmp-kvm/drivers/misc/tcioDD/tcServiceCodes.h
@@ -0,0 +1,64 @@
+//
+//  File: tcServiceCodes.h
+//  Description: tcService Codes
+//
+//  Copyright (c) 2012, John Manferdelli.  All rights reserved.
+//     Some contributions Copyright (c) 2012, Intel Corporation. 
+//
+// Use, duplication and disclosure of this file and derived works of
+// this file are subject to and licensed under the Apache License dated
+// January, 2004, (the "License").  This License is contained in the
+// top level directory originally provided with the CloudProxy Project.
+// Your right to use or distribute this file, or derived works thereof,
+// is subject to your being bound by those terms and your use indicates
+// consent to those terms.
+//
+// If you distribute this file (or portions derived therefrom), you must
+// include License in or with the file and, in the event you do not include
+// the entire License in the file, the file must contain a reference
+// to the location of the License.
+
+
+// ------------------------------------------------------------------------------
+
+
+#ifndef __TCSERVICECODES_H__
+#define __TCSERVICECODES_H__
+
+
+#define TCSERVICEGETPOLICYKEYFROMAPP            1
+#define TCSERVICEGETPOLICYKEYFROMTCSERVICE      2
+
+#define TCSERVICEGETOSHASHFROMAPP               3
+#define TCSERVICEGETOSHASHFROMTCSERVICE         4
+
+#define TCSERVICEGETOSCREDSFROMAPP              5
+#define TCSERVICEGETOSCREDSFROMTCSERVICE        6
+
+#define TCSERVICEGETPROGHASHFROMAPP             7
+#define TCSERVICEGETPROGHASHFROMTCSERVICE       8
+
+#define TCSERVICESEALFORFROMAPP                 9
+#define TCSERVICESEALFORFROMTCSERVICE          10
+
+#define TCSERVICEUNSEALFORFROMAPP              11
+#define TCSERVICEUNSEALFORFROMTCSERVICE        12
+
+#define TCSERVICEATTESTFORFROMAPP              13
+#define TCSERVICEATTESTFORFROMTCSERVICE        14
+
+#define TCSERVICESTARTAPPFROMAPP               15
+#define TCSERVICESTARTAPPFROMTCSERVICE         16
+
+#define TCSERVICETERMINATEAPPFROMAPP           17
+#define TCSERVICETERMINATEAPPFROMTCSERVICE     18
+
+#define TCSERVICETERMINATE                    100
+
+
+#endif
+
+
+// ------------------------------------------------------------------------------
+
+

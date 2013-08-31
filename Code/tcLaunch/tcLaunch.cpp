1a07dd62647e90cc8c54d9d6f705fdb730e9ec0e
John Manferdelli <jlmucbmath@gmail.com>
2013-08-31 15:50:02 -0700

    smaller (not tested) launchwq

--- Code/tcLaunch/tcLaunch.cpp
+++ Code/tcLaunch/tcLaunch.cpp
@@ -24,13 +24,11 @@
 
 
 #include "jlmTypes.h"
-#include "jlmUtility.h"
 #include "logging.h"
-#include "channel.h"
+#include "tcIO.h"
+#include "tcServiceCodes.h"
+#include "buffercoding.h"
 #include "tinyxml.h"
-#include "request.h"
-#include "linuxHostsupport.h"
-#include "kvmHostsupport.h"
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -67,31 +65,245 @@ const char* g_tcioDDName= "ktciodd0";
 const char* g_tcioDDName= "tcioDD0";
 #endif
 
+int         g_myPid;
+
 
 // ------------------------------------------------------------------------
 
 
-
-bool startAsVm(int an, char** av)
+void tcBufferprint(tcBuffer* p)
 {
-    int     n= 0;
+    fprintf(g_logFile, 
+           "Buffer: procid: %ld, req: %ld, size: %ld, status: %ld, orig proc: %ld\n",
+           (long int)p->m_procid, (long int)p->m_reqID, (long int)p->m_reqSize,
+           (long int)p->m_ustatus, (long int)p->m_origprocid);
+}
+ 
 
-    UNUSEDVAR(n);
-//    if(!initLinuxService(g_tcioDDName))
-//        return false;
- //   if(!startAppfromDeviceDriver(av[0], &n, an, av))
-  //      return false;
+bool gettcBuf(int fd, int* procid, u32* puReq, u32* pstatus, int* porigprocid,
+                         int* paramsize, byte* params)
+{
+    byte            rgBuf[PADDEDREQ];
+    tcBuffer*       pReq= (tcBuffer*) rgBuf;
+    int             i;
+    int             n;
+
+#ifdef TEST
+    fprintf(g_logFile, "gettcBuf outstanding %d\n", fd);
+#endif
+    n= *paramsize+sizeof(tcBuffer);
+    if(n>PADDEDREQ) {
+        fprintf(g_logFile, "Buffer too small\n");
+        return false;
+    }
+    i= read(fd, rgBuf, n);
+    if(i<0) {
+        fprintf(g_logFile, "ReadBufRequest failed in gettcBuf %d %d\n", i, n);
+        return false;
+    }
+
+#ifdef TEST
+    fprintf(g_logFile, "gettcBuf succeeds %d\n", i);
+    tcBufferprint((tcBuffer*) rgBuf);
+    PrintBytes("Buffer: ", rgBuf, i);
+#endif
+
+#ifdef HEADERTEST
+    fprintf(g_logFile, "gettcBuf: "); pReq->print();
+#endif
+    *procid= pReq->m_procid;
+    *puReq= pReq->m_reqID;
+    *pstatus= pReq->m_ustatus;
+    *porigprocid= pReq->m_origprocid;
+    i-= sizeof(tcBuffer);
+    if(*paramsize<i)
+        return false;
+    *paramsize= i;
+    memcpy(params, &rgBuf[sizeof(tcBuffer)], i);
+
     return true;
 }
 
 
-bool startAsMeasuredProgram(int an, char** av)
+bool sendtcBuf(int fd, int procid, u32 uReq, u32 status, int origproc,
+                          int paramsize, byte* params)
+{
+#if 1
+    byte            rgBuf[PADDEDREQ];
+    tcBuffer*       pReq= (tcBuffer*) rgBuf;
+    int             i;
+    int             n;
+
+    if(paramsize>(PARAMSIZE)) {
+        fprintf(g_logFile, "sendtcBuf buffer too small %d\n", paramsize);
+        return false;
+    }
+    pReq->m_procid= procid;
+    pReq->m_reqID= uReq;
+    pReq->m_ustatus= status;
+    pReq->m_origprocid= origproc;
+    pReq->m_reqSize= paramsize;
+#ifdef HEADERTEST
+    fprintf(g_logFile, "sendtcBuf: "); pReq->print();
+#endif
+    n= paramsize+sizeof(tcBuffer);
+    memcpy(&rgBuf[sizeof(tcBuffer)], params, paramsize);
+    i= write(fd, rgBuf, n);
+    if(i<0) {
+        fprintf(g_logFile, "tcChannel::sendtcBuf: WriteBufRequest failed %d\n", i);
+        return false;
+    }
+#endif
+    return true;
+}
+
+
+bool startAppfromDeviceDriver(int fd, const char* szexecFile, int* ppid,
+    int argc, char **argv)
+{
+#if 1
+    u32         ustatus;
+    u32         ureq;
+    int         procid;
+    int         origprocid;
+    int         size= PARAMSIZE;
+    byte        rgBuf[PARAMSIZE];
+
+    size= encodeTCSERVICESTARTAPPFROMAPP(szexecFile, argc, argv, PARAMSIZE, rgBuf);
+    if(size<0) {
+        fprintf(g_logFile, "startProcessLinuxService: encodeTCSERVICESTARTAPPFROMAPP failed\n");
+        return false;
+    }
+    if(!sendtcBuf(fd, g_myPid, TCSERVICESTARTAPPFROMAPP, 0, g_myPid, size, rgBuf)) {
+        fprintf(g_logFile, "startProcessLinuxService: sendtcBuf for TCSERVICESTARTAPPFROMAPP failed\n");
+        return false;
+    }
+    size= PARAMSIZE;
+    if(!gettcBuf(fd, &procid, &ureq, &ustatus, &origprocid, &size, rgBuf)) {
+        fprintf(g_logFile, "startProcessLinuxService: gettcBuf for TCSERVICESTARTAPPFROMAPP failed\n");
+        return false;
+    }
+    if(!decodeTCSERVICESTARTAPPFROMTCSERVICE(ppid, rgBuf)) {
+        fprintf(g_logFile, "startProcessLinuxService: cant decode childproc\n");
+        return false;
+    }
+#ifdef TEST
+    fprintf(g_logFile, "startProcessLinuxService: Process created: %d by servicepid %d\n", 
+          *ppid, g_myPid);
+#endif
+#endif
+    return true;
+}
+
+
+int initLinuxService()
+{
+#ifdef TEST
+        fprintf(g_logFile, "initLinuxService device %s\n", g_tcioDDName);
+#endif
+
+    int fd= open(g_tcioDDName, O_RDWR);
+        if(fd<0) {
+            fprintf(g_logFile, "Can't open device driver %s\n", g_tcioDDName);
+            return -1;
+        }
+
+#ifdef TEST
+    fprintf(g_logFile, "initLinuxService returns %d\n", fd);
+#endif
+    return fd;
+}
+
+
+// -------------------------------------------------------------------------
+
+
+#ifdef KVMTCSERVICE
+    virConnectPtr       g_vmconnection;
+    virDomainPtr        g_vmdomain;
+#endif
+
+
+int startKvmVM(const char* szvmimage, const char* systemname,
+                const char* xmldomainstring, const char* szdomainName)
+
+// returns -1 if error, vmid otherwise
+
+{
+#ifdef KVMTCSERVICE
+    int     vmid= 0;  //TODO
+    if(szvmimage==NULL || systemname==NULL || xmldomainstring==NULL ||
+                          szdomainName==NULL) {
+        fprintf(g_logFile, "startKvm: Bad input arguments\n");
+        return -1;
+    }
+
+#ifdef TEST
+    fprintf(g_logFile, "startKvm: %s, %s, %s %s\n", 
+               szvmimage, systemname, xmldomainstring, szdomainName);
+#endif
+
+    // Replace later with virConnectOpenAuth(name, virConnectAuthPtrDefault, 0)
+    g_vmconnection= virConnectOpen(systemname);
+    if(g_vmconnection==NULL) {
+        fprintf(g_logFile, "startKvm: couldn't connect\n");
+        return -1;
+    }
+
+#ifdef TEST
+    char*   szCap= virConnectGetCapabilities(g_vmconnection);
+    fprintf(g_logFile, "VM Host capabilities:\n%s\n", szCap);
+    free(szCap);
+    char*   szHostname= virConnectGetHostname(g_vmconnection);
+    fprintf(g_logFile, "Host name: %s\n", szHostname);
+    free(szszHostname);
+    int ncpus= virConnectGetMaxVcpus(g_vmconnection);
+    fprintf(g_logFile, "Virtual cpus: %d\n", ncpus);
+    virNodeInfo oInfo;
+    virNodeGetInfo(g_vmconnection, &oInfo);
+    fprintf(g_logFile, "\tModel : %s\n", oInfo.model);
+    fprintf(g_logFile, "\tMemory size: %d\n", oInfo.memory);
+    fprintf(g_logFile, "\tNum cpus: %d\n", oInfo.cpus);
+#if 0
+    virErrorPtr  err;
+    err= virGetLastError();
+    fprintf(g_logFile, "Error: %s\n", err->message);
+#endif
+#endif
+   
+    g_vmdomain= virDomainCreateXML(g_vmconnection, xmldomainstring, 0); 
+    if(g_vmdomain==NULL) {
+        fprintf(g_logFile, "startKvm: couldn't start domain\n");
+        return -1;
+    }
+#ifdef TEST
+    int     ndom= virConnectNumDomains(g_vmconnection);
+    fprintf(g_logFile, "%d domains\n", ndom);
+
+    int*    rgactiveDomains= malloc(sizeof(int)*ndom);
+    int     i;
+    ndom= virConnectListDomains(g_vmconnection, rgactiveDomains, ndom);
+    fprintf(g_logFile, "active domains\n");
+    for(i=0; i<ndom; i++) {
+        fprintf(g_logFile, "\t%d\n", activeDomains[i]);
+    }
+#endif
+
+    return vmid;
+#else
+    return -1;
+#endif
+}
+
+
+// ------------------------------------------------------------------------
+
+
+bool startAsMeasuredProgram(int fd, int an, char** av)
 {
     int     n= 0;
 
-    if(!initLinuxService(g_tcioDDName))
-        return false;
-    if(!startAppfromDeviceDriver(av[0], &n, an, av))
+    if(!startAppfromDeviceDriver(fd, av[0], &n, an, av))
         return false;
     return true;
 }
@@ -102,11 +314,6 @@ bool startAsMeasuredProgram(int an, char** av)
 
 int main(int an, char** av)
 {
-    int             i;
-    const char*     directory= NULL;
-    char*           szPort= (char*)"";
-    char*           szAddress= (char*)"";
-    char*           parameter= (char*)"";
     char*           program= (char*)"";
 
 #ifdef  TEST
@@ -114,41 +321,37 @@ int main(int an, char** av)
     fprintf(g_logFile, "tcLaunch test\n");
     fflush(g_logFile);
 #endif
-    UNUSEDVAR(directory);
-    UNUSEDVAR(szPort);
-    UNUSEDVAR(szAddress);
+#if 0
+    int             i;
+    const char*     directory= NULL;
     if(an>1) {
         for(i=0;i<an;i++) {
-            if(strcmp(av[i],"-port")==0 && an>(i+1)) {
-                szPort= strdup(av[++i]);
-            }
-            if(strcmp(av[i],"-address")==0) {
-                szAddress= strdup(av[++i]);
-            }
             if (strcmp(av[i],"-directory")==0) {
                 directory= strdup(av[++i]);
             }
         }
     }
+#endif
 
     initLog("tcLaunch.log");
+    g_myPid= getpid();
 
 #ifdef  TEST
     fprintf(g_logFile, "tcLaunch main starting measured %s\n", av[0]);
 #endif
 
-     if(!initLinuxService(parameter)) {
-        fprintf(g_logFile, "taoHostServices::HostInit: cant init Linuxservice\n");
-        return false;
+    int fd= initLinuxService();
+    if(fd<0) {
+        return 1;
     }
     int   handle= 0;
-
-    startAppfromDeviceDriver(program, &handle, an, av);
+    startAppfromDeviceDriver(fd, program, &handle, an, av);
 
 #ifdef TEST
     fprintf(g_logFile, "main: measured program started, exiting\n");
     fflush(g_logFile);
 #endif
+    close(fd);
     closeLog();
     return 0;
 }
--- Code/tcLaunch/tcLaunch.mak
+++ Code/tcLaunch/tcLaunch.mak
@@ -6,7 +6,6 @@ BN=         ../jlmbignum
 SCD=        ../jlmcrypto
 TAO=	    ../tao
 TRS=	    ../tcService
-CH=	    ../channels
 PROTO=	    ../protocolChannel
 CLM=	    ../claims
 
@@ -19,10 +18,8 @@ LDFLAGS          := $(RELEASE_LDFLAGS)
 CC=         g++
 LINK=       g++
 
-dobjs=      $(B)/jlmUtility.o $(B)/linuxHostsupport.o \
-	    $(B)/tinyxml.o $(B)/tinyxmlparser.o $(B)/tinystr.o \
-	    $(B)/tinyxmlerror.o $(B)/channel.o $(B)/request.o \
-	    $(B)/buffercoding.o $(B)/tcIO.o $(B)/tcLaunch.o $(B)/logging.o 
+dobjs=      $(B)/tinyxml.o $(B)/tinyxmlparser.o $(B)/tinystr.o $(B)/tinyxmlerror.o \
+	    $(B)/buffercoding.o $(B)/tcLaunch.o $(B)/logging.o 
 
 all: $(E)/tcLaunch.exe
 
@@ -32,18 +29,6 @@ $(E)/tcLaunch.exe: $(dobjs)
 
 $(B)/logging.o: $(SC)/logging.cpp $(SC)/logging.h
 	$(CC) $(CFLAGS) -I$(SC) -c -o $(B)/logging.o $(SC)/logging.cpp
-
-$(B)/jlmUtility.o: $(SC)/jlmUtility.cpp $(SC)/jlmUtility.h
-	$(CC) $(CFLAGS) -I$(SC) -I$(SCD) -I$(BN) -c -o $(B)/jlmUtility.o $(SC)/jlmUtility.cpp
-
-$(B)/linuxHostsupport.o: $(TAO)/linuxHostsupport.cpp $(TAO)/linuxHostsupport.h
-	$(CC) $(CFLAGS) -I$(SC) -I$(SCD) -I$(BN) -I$(TAO) -I$(TRS) -c -o $(B)/linuxHostsupport.o $(TAO)/linuxHostsupport.cpp
-
-$(B)/kvmHostsupport.o: $(TAO)/kvmHostsupport.cpp $(TAO)/kvmHostsupport.h
-	$(CC) $(CFLAGS) -I$(SC) -I$(SCD) -I$(BN) -I$(TAO) -c -o $(B)/kvmHostsupport.o $(TAO)/kvmHostsupport.cpp
-
-$(B)/request.o: $(PROTO)/request.cpp $(PROTO)/request.h
-	$(CC) $(CFLAGS) -I$(SC) -I$(SCD) -I$(BN) -I$(CH) -I$(CLM) -c -o $(B)/request.o $(PROTO)/request.cpp
 
 $(B)/tinyxml.o : $(SC)/tinyxml.cpp $(SC)/tinyxml.h $(SC)/tinystr.h
 	$(CC) $(CFLAGS) $(RELEASECFLAGS) -I$(SC) -c -o $(B)/tinyxml.o $(SC)/tinyxml.cpp
@@ -63,9 +48,6 @@ $(B)/tcIO.o: $(TRS)/tcIO.cpp $(TRS)/tcIO.h
 $(B)/buffercoding.o: $(TRS)/buffercoding.cpp $(TRS)/buffercoding.h
 	$(CC) $(CFLAGS) -I$(SC) -I$(SCD) -I$(TAO) -I$(BN) -I$(TRS) -c -o $(B)/buffercoding.o $(TRS)/buffercoding.cpp
 
-$(B)/channel.o: $(CH)/channel.cpp $(CH)/channel.h
-	$(CC) $(CFLAGS) -I$(SC) -c -o $(B)/channel.o $(CH)/channel.cpp
-
 $(B)/tcLaunch.o: $(S)/tcLaunch.cpp
-	$(CC) $(CFLAGS) -I$(BN) -I$(PROTO) -I$(CH) -I$(TAO) -I$(SCD) -I$(CLM) -I$(TRS) -I$(SC) -D LINUXTCSERVICE -c -o $(B)/tcLaunch.o $(S)/tcLaunch.cpp
+	$(CC) $(CFLAGS) -I$(BN) -I$(PROTO) -I$(TAO) -I$(SCD) -I$(CLM) -I$(TRS) -I$(SC) -D LINUXTCSERVICE -c -o $(B)/tcLaunch.o $(S)/tcLaunch.cpp
 

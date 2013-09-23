09940816cde0eefcebcf28859a45051580c4cfc6
John Manferdelli <jlmucbmath@gmail.com>
2013-09-23 11:28:06 -0700

    New test.sh in Supportfiles demonstrating tcLaunch for OS and Hv

--- Code/SupportFiles/test.sh
+++ Code/SupportFiles/test.sh
@@ -1,14 +1,20 @@
 sudo bash
-cd ~/jlmcrypt
 /etc/init.d/trousers stop
 chown jlm /dev/tpm0
+cd ~/jlmcrypt
+# only do the following if the drivers is not loaded
 insmod tcioDD.ko
 chmod 0777 /dev/tcioDD0
-exit
-./tcService.exe &
 ./keyNegoServer.exe
+./tcService.exe -initKeys
+./tcService.exe &
 sleep 2s
-./fileServer.exe -initProg
+./tcLaunch.exe -LinuxGuest ./fileServer.exe
 sleep 2s
-./fileClient.exe -initProg
+tcLaunch.exe -LinuxGuest ./fileClient.exe
+sleep 5s
+
+#this is the standalone test in the kvm host
+./tcKvmService.exe &
+./tcLaunch.exe -KVMHost Test1 /home/jlm/jlmcrypt/vms/vmlinuz-3.5.0-23-generic /home/jlm/jlmcrypt/vms/initrd.img-3.5.0-23-generic /home/jlm/jlmcrypt/vms/Test1.img
 

2e8015b074a0492969c86a70ff540bb614878eca
John Manferdelli <jlmucbmath@gmail.com>
2013-10-05 10:52:28 -0700

    update scripts

--- Code/SupportFiles/suGuestPrep.sh
+++ Code/SupportFiles/suGuestPrep.sh
@@ -1,9 +1,7 @@
 #
 #    This is run as root to prepare the KVMGuest
-#    and start the service.  TODO: take argumments
+#    and init the service.  TODO: take argumments
 #
-/etc/init.d/trousers stop
-chown jlm /dev/tpm0
 chmod 0777 /dev/*tcio*
 ./tcKvmGuestOsService.exe -initKeys
 
--- Code/SupportFiles/suInitGuest.sh
+++ Code/SupportFiles/suInitGuest.sh
@@ -2,7 +2,7 @@
 #    This is run as root to Init the Guest's
 #    tcService
 #
-
+chmod 0777 /dev/*tcio*
 ./keyNegoServer.exe &
 ./tcKvmGuestOsService.exe -initKeys
 

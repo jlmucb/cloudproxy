c13507e130e40bf0ba8e3525085bd853d9576ccd
John Manferdelli <jlmucbmath@gmail.com>
2013-12-08 16:57:53 -0800

    new versions of toBase64 and fromBase64

--- Code/jlmcrypto/jlmcrypto.cpp
+++ Code/jlmcrypto/jlmcrypto.cpp
@@ -103,228 +103,213 @@ bool closeAllCrypto()
 
 // -----------------------------------------------------------------------------
 
+
 // pad character is '='
 static const char* s_transChar= 
           "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-static const byte s_revTrans[80]= {
-    62,  0,  0,  0, 63,
-    52, 53, 54, 55, 56,
-    57, 58, 59, 60, 61,
-     0,  0,  0,  0,  0,
-     0,  0,  0,  1,  2,
-     3,  4,  5,  6,  7,
-     8,  9, 10, 11, 12,
-    13, 14, 15, 16, 17,
-    18, 19, 20, 21, 22,
-    23, 24, 25,  0,  0,
-     0,  0,  0,  0, 26,
-    27, 28, 29, 30, 31,
-    32, 33, 34, 35, 36,
-    37, 38, 39, 40, 41,
-    42, 43, 44, 45, 46,
-    47, 48, 49, 50, 51,
-};
 
-
-inline bool whitespace(char b)
+inline int numbytesfromBase64string(int nc)
 {
-    return b==' ' || b=='\t' || b=='\r' || b=='\n';
+    return (6*nc+NBITSINBYTE-1)/NBITSINBYTE;
 }
 
 
-// -------------------------------------------------------------------------------
-
-
-bool toBase64(int inLen, const byte* pbIn, int* poutLen, char* szOut, bool fDirFwd)
-//
-//      Lengths are in characters
-//
+inline int numbase64charsfrombytes(int nb)
 {
-    int             numOut= ((inLen*4)+2)/3;
-    int             i= 0;
-    int             a, b, c, d;
-    const byte*     pbC;
+    return (NBITSINBYTE*nb+5)/6;
+}
 
-    // enough room?
-    if(numOut>*poutLen)
-        return false;
 
-    if(fDirFwd) {
-        pbC= pbIn+inLen-1;
-        while(inLen>2) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC-1)>>4)&0xf);
-            c= ((*(pbC-1)&0xf)<<2) | ((*(pbC-2)>>6)&0x3);
-            d= (*(pbC-2)&0x3f);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= s_transChar[c];
-            szOut[i++]= s_transChar[d];
-            pbC-= 3;
-            inLen-= 3;
-        }
-        // 8 bits left
-        if(inLen==1) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC-1)>>4)&0xf);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= '=';
-            szOut[i++]= '=';
-        }
-        // 16 bits left
-        if(inLen==2) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC-1)>>4)&0xf);
-            c= ((*(pbC-1)&0xf)<<2);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= s_transChar[c];
-            szOut[i++]= '=';
-        }
+inline byte b64value(char a)
+{
+    if(a>='A'&&a<='Z')
+        return (byte) a-'A';
+    if(a>='a'&&a<='z')
+        return (byte) a-'a'+26;
+    if(a>='0'&&a<='9')
+        return (byte) a-'0'+52;
+    if(a=='+')
+        return 0x3e;
+    if(a=='/')
+        return 0x3f;
+    return 0xff;  // error
+}
+
+
+bool toBase64(int inlen, const byte* in, int* poutlen, char* szout, bool dir)
+{
+    int	    numout= numbase64charsfrombytes(inlen);
+    
+    if(numout>*poutlen)
+	return false;
+
+    int	        n= inlen;
+    int         a, b, c, d;
+    const byte* pb;
+
+    // s_transChar
+    if(dir) {
+	// scan from high order byte to low
+	// 24 bit chunks
+        pb= in+inlen-1;     // start at high order byte (eg-on little endian machine)
+	while(n>2) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb-1)>>4)&0xf);
+	    c= (((*(pb-1))&0xf)<<2)|((*(pb-2)>>6)&0x3);
+	    d= (*(pb-2))&0x3f;
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= s_transChar[c];
+	    *(szout++)= s_transChar[d];
+	    n-= 3;
+	    pb-= 3;
+	}
+	// 16 bits left
+	if(n==2) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb-1)>>4)&0xf);
+	    c= (((*(pb-1))&0xf)<<2);
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= s_transChar[c];
+	    *(szout++)= '=';
+	    n= 0;
+	}
+	// 8 bits left
+	if(n==1) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb-1)>>4)&0xf);
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= '=';
+	    *(szout++)= '=';
+	    n= 0;
+	}
     }
     else {
-        pbC= pbIn;
-        while(inLen>2) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC+1)>>4)&0xf);
-            c= ((*(pbC+1)&0xf)<<2) | ((*(pbC+2)>>6)&0x3);
-            d= (*(pbC+2)&0x3f);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= s_transChar[c];
-            szOut[i++]= s_transChar[d];
-            pbC+= 3;
-            inLen-= 3;
-        }
-        // 8 bits left
-        if(inLen==1) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC+1)>>4)&0xf);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= '=';
-            szOut[i++]= '=';
-        }
-        // 16 bits left
-        if(inLen==2) {
-            a= (*pbC>>2)&0x3f;
-            b= ((*pbC&0x3)<<4) | ((*(pbC+1)>>4)&0xf);
-            c= ((*(pbC+1)&0xf)<<2);
-            szOut[i++]= s_transChar[a];
-            szOut[i++]= s_transChar[b];
-            szOut[i++]= s_transChar[c];
-            szOut[i++]= '=';
-        }
+	// scan from low order byte to high
+	// 24 bit chunks
+        pb= in;
+	while(n>2) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb+1)>>4)&0xf);
+	    c= (((*(pb+1))&0xf)<<2)|((*(pb+2)>>6)&0x3);
+	    d= (*(pb+2))&0x3f;
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= s_transChar[c];
+	    *(szout++)= s_transChar[d];
+	    n-= 3;
+	    pb+= 3;
+	}
+	// 16 bits left
+	if(n==2) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb+1)>>4)&0xf);
+	    c= (((*(pb+1))&0xf)<<2);
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= s_transChar[c];
+	    *(szout++)= '=';
+	    n= 0;
+	}
+	// 8 bits left
+	if(n==1) {
+	    a= ((*pb)>>2)&0x3f;
+	    b= (((*pb)&0x3)<<4)|((*(pb+1)>>4)&0xf);
+	    *(szout++)= s_transChar[a];
+	    *(szout++)= s_transChar[b];
+	    *(szout++)= '=';
+	    *(szout++)= '=';
+	    n= 0;
+	}
     }
-    *poutLen= i;
-    szOut[i++]= 0;
+    *szout= 0;
+    *poutlen= numout;
     return true;
 }
 
 
-bool fromBase64(int inLen, const char* szIn, int* poutLen, unsigned char* puOut, bool fDirFwd)
-//
-//      Lengths are in characters
-//
+bool fromBase64(int inlen, const char* szin, int* poutlen, byte* out, bool dir)
 {
-    int             numOut= ((inLen*3)+3)/4;
-    unsigned char*  puW;
-    unsigned char   a,b,c,d;
-    int             numLeft= inLen;
+    int	    numout= numbytesfromBase64string(inlen);
 
-    if(inLen>2 && *(szIn+inLen-1)=='=')
-        numOut--;
-    if(inLen>2 && *(szIn+inLen-2)=='=')
-        numOut--;
-    puW= puOut+numOut-1;
-
-    // enough room?
-    if(numOut>*poutLen) {
+    if(inlen<4 || (inlen%4)!=0)
         return false;
-    }
 
-    while(numLeft>3) {
-        while(whitespace(*szIn) && numLeft>0) {
-            szIn++; numLeft--;
-        }
-        if(*szIn<43 || *szIn>122) {
-            return false;
-        }
-        a= s_revTrans[*szIn-43];
-        szIn++; numLeft--;
-        while(whitespace(*szIn) && numLeft>0) {
-            szIn++; numLeft--;
-        }
-        if(*szIn<43 || *szIn>122) {
-            return false;
-        }
-        b= s_revTrans[*szIn-43];
-        szIn++; numLeft--;
-        while(whitespace(*szIn) && numLeft>0) {
-            szIn++; numLeft--;
-        }
-        if(*szIn=='=') {
-            if(!fDirFwd) {
-                *(puOut)= (a<<2) | (b>>4);
-                puOut+= 1;
+    // does padding affect output length?
+    if(*(szin+inlen-1)=='=')
+        numout--;
+    if(*(szin+inlen-2)=='=')
+        numout--;
+
+    if(numout>*poutlen)
+	return false;
+
+    const char* p= szin;
+    byte*   pb= out;
+    byte    a, b, c, d;
+    if(dir) {
+        pb+= numout-1;
+        while(*p!='\0') {
+            a= b64value(*p++);
+            if(a==0xff)
+                return false;
+            b= b64value(*p++);
+            if(b==0xff)
+                return false;
+            if(*(p+1)=='=') {
+                *pb--= a<<2|b>>4;
+                if(*p!='=') {
+                    c= b64value(*p++);
+                    if(c==0xff)
+                        return false;
+                    *pb--= b<<4|c;
+                }
+                break;
             }
-            else {
-                *(puW)= (a<<2) | (b>>4);
-                puW-= 1;
-            }
-            numLeft-= 2;
-            continue;
-        }
-        if(*szIn<43 || *szIn>122) {
-            return false;
-        }
-        c= s_revTrans[*szIn-43];
-        szIn++; numLeft--;
-        while(whitespace(*szIn) && numLeft>0) {
-            szIn++; numLeft--;
-        }
-        if(*szIn=='=') {
-            if(!fDirFwd) {
-                *(puOut)= (a<<2) | (b>>4);
-                *(puOut+1)= ((b&0xf)<<4) | (c>>2);
-                puOut+= 2;
-            }
-            else {
-                *(puW)= (a<<2) | (b>>4);
-                *(puW-1)= ((b&0xf)<<4) | (c>>2);
-                puW-= 2;
-            }
-            numLeft-= 1;
-            continue;
-        }
-        if(*szIn<43 || *szIn>122) {
-            return false;
-        }
-        d= s_revTrans[*szIn-43];
-        szIn++; numLeft--;
-        if(!fDirFwd) {
-            *(puOut)= (a<<2) | (b>>4);
-            *(puOut+1)= ((b&0xf)<<4) | (c>>2);
-            *(puOut+2)= ((c&0x3)<<6) | d;
-            puOut+= 3;
-        }
-        else {
-            *(puW)= (a<<2) | (b>>4);
-            *(puW-1)= ((b&0xf)<<4) | (c>>2);
-            *(puW-2)= ((c&0x3)<<6) | d;
-            puW-= 3;
+            c= b64value(*p++);
+            if(c==0xff)
+                return false;
+            d= b64value(*p++);
+            if(d==0xff)
+                return false;
+            *pb--= a<<2|(b>>4);
+            *pb--= b<<4|(c>>2);
+            *pb--= c<<6|d;
         }
     }
-
-    while(whitespace(*szIn) && numLeft>0) {
-        szIn++; numLeft--;
+    else {
+        while(*p!='\0') {
+            a= b64value(*p++);
+            if(a==0xff)
+                return false;
+            b= b64value(*p++);
+            if(b==0xff)
+                return false;
+            if(*(p+1)=='=') {
+                *pb++= a<<2|b>>4;
+                if(*p!='=') {
+                    c= b64value(*p++);
+                    if(c==0xff)
+                        return false;
+                    *pb++= b<<4|c>>2;
+                }
+                break;
+            }
+            c= b64value(*p++);
+            if(c==0xff)
+                return false;
+            d= b64value(*p++);
+            if(d==0xff)
+                return false;
+            *pb++= a<<2|(b>>4);
+            *pb++= b<<4|(c>>2);
+            *pb++= c<<6|d;
         }
-    if(numLeft>0) {
-        return false;
     }
 
-    *poutLen= numOut;
+    *poutlen= numout;
     return true;
 }
 

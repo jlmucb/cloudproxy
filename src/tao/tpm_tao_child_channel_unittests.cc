c1fb053db4f84577c6096be80f4b85ca36e13831
Tom Roeder <tmroeder@google.com>
2014-01-03 10:34:56 -0800

    Added gflags parsing to the main file for the tpm test.

--- src/tao/tpm_tao_child_channel_unittests.cc
+++ src/tao/tpm_tao_child_channel_unittests.cc
@@ -72,6 +72,7 @@ TEST_F(TPMTaoChildChannelTest, AttestTest) {
 }
 
 GTEST_API_ int main(int argc, char **argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();

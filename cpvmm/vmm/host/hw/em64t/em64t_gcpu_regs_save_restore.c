b3f4bfc3a3deaec7cd13afa3157ba4e6508503a7
Rekha Bachwani <bachwani@gmail.com>
2014-01-18 05:58:32 -0800

    C inline assembly

---
+++ cpvmm/vmm/host/hw/em64t/em64t_gcpu_regs_save_restore.c
@@ -0,0 +1,251 @@
+/*
+#
+# Copyright (c) 2013 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+*/
+
+//RNB: Adding header for GUEST_CPU_SAVE_AREA and hw_cpu_id().
+#include "../../../guest/guest_cpu/guest_cpu_internal.h"
+// Assumption - hw_cpu_id() uses RAX only and returns host cpu id in ax
+//extern hw_cpu_id();
+
+// pointer to the array of pointers to the GUEST_CPU_SAVE_AREA_PREFIX
+extern GUEST_CPU_SAVE_AREA** g_guest_regs_save_area;
+
+// include       ia32_registers.equ
+
+// Define initial part of GUEST_CPU_SAVE_AREA structure
+typedef struct {
+    VMM_GP_REGISTERS gp; 
+    VMM_XMM_REGISTERS xmm;
+} GUEST_CPU_SAVE_AREA_PREFIX; 
+
+/*
+* Load pointer to the active GUEST_CPU_SAVE_AREA_PREFIX into rbx
+* No other registers are modified
+*/
+void load_save_area_into_rbx(void) {
+
+	int cpuid;
+    // save RAX temporary
+    // calculate host cpu id and put it into the rax (ax)
+    cpuid = hw_cpu_id();
+    	asm("push %rax \n\t"
+//RNB: mov rax is probably not required, as the rax (cpuid) should do the trick.
+//					"mov %rax, %0 \n\t"
+    // put pointer to the array of GUEST_CPU_SAVE_AREA_PREFIX* to RBX
+    			"mov  %rbx, g_guest_regs_save_area \n\t"
+    			"mov  %rbx, (%rbx) \n\t"
+    // put pointer to our GUEST_CPU_SAVE_AREA_PREFIX struct to RBX
+    			"mov  %rbx, (%rbx + sizeof qword * %rax) \n\t"
+    // restore RAX
+    			"pop %rax"
+//RNB: this function has no output, but the below line is to satisfy the compiler
+					:"=g" (cpuid)
+					:"rax" (cpuid)
+				  :"rax", "rbx");
+/*
+    // put pointer to the array of GUEST_CPU_SAVE_AREA_PREFIX* to RBX
+    asm("mov  %rbx, g_guest_regs_save_area");
+    asm("mov  %rbx, (%rbx)");
+    // put pointer to our GUEST_CPU_SAVE_AREA_PREFIX struct to RBX
+    asm("mov  %rbx, (%rbx + sizeof qword * %rax)");
+    // restore RAX
+    asm("pop %rax");
+*/
+    return;
+}
+/*
+* This functions are part of the GUEST_CPU class.
+* They are called by assembler-lever VmExit/VmResume functions
+* to save all registers that are not saved in VMCS but may be used immediately
+* by C-language VMM code.
+# The following registers are NOT saved here
+#
+#   RIP            part of VMCS
+#   RSP            part of VMCS
+#   RFLAGS         part of VMCS
+#   segment regs   part of VMCS
+#   control regs   saved in C-code later
+#   debug regs     saved in C-code later
+#   FP/MMX regs    saved in C-code later
+#
+# Assumptions:
+#   No free registers except of RSP/RFLAGS
+#   FS contains host CPU id (should be calculated)
+#
+#
+
+#
+# Assumption - no free registers on entry, all are saved on exit
+#
+*/
+void gcpu_save_registers(void) {
+    // save RAX and RBX temporary on a stack
+		GUEST_CPU_SAVE_AREA_PREFIX *gsap;
+//		int gp_count = IA32_REG_GP_COUNT;
+//		int xmm_count = IA32_REG_XMM_REGISTERS;
+    asm("push %rbx");
+    // put pointer to our GUEST_CPU_SAVE_AREA_PREFIX struct to RBX
+    load_save_area_into_rbx();
+    // now save rax and rbx first
+		
+		asm("mov %0, %rbx \n\t" // this moves %rbx to gsap
+				"mov [%rbx], %rax \n\t"
+				"pop %rax \n\t" // this is %rbx
+				"mov 8[%rbx], %rax \n\t"
+				"mov 16[%rbx], %rcx \n\t"
+				"mov 24[%rbx], %rdx \n\t"
+				"mov 32[%rbx], %rdi \n\t"
+				"mov 40[%rbx], %rsi \n\t"
+				"mov 48[%rbx], %rbp \n\t"
+				"mov 64[%rbx], %r8 \n\t"
+				"mov 72[%rbx], %r9 \n\t"
+				"mov 80[%rbx], %r10 \n\t"
+				"mov 88[%rbx], %r11 \n\t"
+				"mov 96[%rbx], %r12 \n\t"
+				"mov 104[%rbx], %r13 \n\t"
+				"mov 112[%rbx], %r14 \n\t"
+				"mov 120[%rbx], %r15 \n\t"
+	/* now save XMM registers
+	 * Depending on the compiler used, not all XMMs are needed to save/restore
+	 * Before any release, use dumpbin.exe to examine asm code and remove
+	 * the unused XMMs.
+	*/
+//RNB: instead of using 144...182 as offset, it should be IA32_REG_GP_COUNT*8
+				"movaps 144[%rbx], %%xmm0 \n\t"
+				"movaps 152[%rbx], %%xmm1 \n\t"
+				"movaps 160[%rbx], %%xmm2 \n\t"
+				"movaps 168[%rbx], %%xmm3 \n\t"
+				"movaps 176[%rbx], %%xmm4 \n\t"
+				"movaps 182[%rbx], %%xmm5 \n\t"
+//RNB: in the next two  lines (gsap) is to satisfy gcc
+				:"=r" (gsap)
+				:"r" (gsap)
+				:
+		);
+
+	/*
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RAX], %rax");
+    asm("pop  %rax"); //   # this is rbx
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RBX], %rax");
+    // now save all other GP registers except of RIP,RSP,RFLAGS
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RCX], %rcx");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RDX], %rdx");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RDI], %rdi");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RSI], %rsi");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RBP], %rbp");
+    // skip RSP
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R8], %r8");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R9], %r9");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R10], %r10");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R11], %r11");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R12], %r12");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R13], %r13");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R14], %r14");
+    asm("mov  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R15], %r15");
+    // skip RIP
+    // skip RFLAGS
+*/     
+
+	/* now save XMM registers
+	 * Depending on the compiler used, not all XMMs are needed to save/restore
+	 * Before any release, use dumpbin.exe to examine asm code and remove
+	 * the unused XMMs.
+	*/
+		
+/*
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM0], %xmm0");
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM1], %xmm1");
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM2], %xmm2");
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM3], %xmm3");
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM4], %xmm4");
+    asm("movaps (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM5], %xmm5");
+	*/
+    
+    // done
+    return;
+}
+
+/*
+ * Assumption - all free registers on entry, no free registers on exit
+*/
+void gcpu_restore_registers(void) { 
+    // put pointer to our GUEST_CPU_SAVE_AREA_PREFIX struct to RBX
+		int dummy;
+    load_save_area_into_rbx();
+    // restore all XMM first
+		asm("movaps %%xmm0, 144[%rbx] \n\t"
+				"movaps %%xmm1, 152[%rbx] \n\t"
+				"movaps %%xmm2, 160[%rbx] \n\t"
+				"movaps %%xmm3, 168[%rbx] \n\t"
+				"movaps %%xmm4, 176[%rbx] \n\t"
+				"movaps %%xmm5, 182[%rbx] \n\t"
+				"mov [%rbx], %rax \n\t"
+				// RNB: rbx is restored at the end
+				"mov 16[%rbx], %rcx \n\t"
+				"mov 24[%rbx], %rdx \n\t"
+				"mov 32[%rbx], %rdi \n\t"
+				"mov 40[%rbx], %rsi \n\t"
+				"mov 48[%rbx], %rbp \n\t"
+				// RNB: rsp is not restored
+				"mov 64[%rbx], %r8 \n\t"
+				"mov 72[%rbx], %r9 \n\t"
+				"mov 80[%rbx], %r10 \n\t"
+				"mov 88[%rbx], %r11 \n\t"
+				"mov 96[%rbx], %r12 \n\t"
+				"mov 104[%rbx], %r13 \n\t"
+				"mov 112[%rbx], %r14 \n\t"
+				"mov 120[%rbx], %r15 \n\t"
+				"mov 8[%rbx], %rbx"
+				:"=r" (dummy)
+				:"r" (dummy)
+				:
+		);
+/*
+    asm("movaps %xmm0, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM0]");
+    asm("movaps %xmm1, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM1]");
+    asm("movaps %xmm2, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM2]");
+    asm("movaps %xmm3, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM3]");
+    asm("movaps %xmm4, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM4]");
+    asm("movaps %xmm5, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).xmm.reg[IA32_REG_XMM5]");
+*/
+	/*
+    // restore all GP except of RBX
+    // now save all other GP registers except of RIP,RSP,RFLAGS
+    asm("mov  %rax, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RAX]");
+    // RBX restore later
+    asm("mov  %rcx, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RCX]");
+    asm("mov  %rdx, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RDX]");
+    asm("mov  %rdi, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RDI]");
+    asm("mov  %rsi, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RSI]");
+    asm("mov  %rbp, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RBP]");
+    // skip RSP
+    asm("mov  %r8,  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R8]");
+    asm("mov  %r9,  (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R9]");
+    asm("mov  %r10, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R10]");
+    asm("mov  %r11, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R11]");
+    asm("mov  %r12, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R12]");
+    asm("mov  %r13, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R13]");
+    asm("mov  %r14, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R14]");
+    asm("mov  %r15, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_R15]");
+    // skip RIP
+    // skip RFLAGS
+    // restore RBX
+    asm("mov  %rbx, (GUEST_CPU_SAVE_AREA_PREFIX ptr [%rbx]).gp.reg[IA32_REG_RBX]");
+*/
+    // done
+    return;
+}
---
+++ cpvmm/vmm/host/hw/em64t/em64t_utils2.c
@@ -0,0 +1,373 @@
+/*
+ * Register usage
+	*
+	* Caller-saved and scratch:
+	*        RAX, RCX, RDX, R8, R9, R10, R11
+	*
+	* Callee-saved
+	*        RBX, RBP, RDI, RSI, R12, R13, R14, and R15
+	*
+	*  void __stdcall
+	*  hw_lgdt (
+	*          void * gdtr
+	*  );
+	*
+	*  Load GDTR (from buffer pointed by RCX)
+	.set    ARG1_U8, %cl
+	.set    ARG1_U16, % cx
+	.set    ARG1_U32, %ecx
+	.set    ARG1_U64, %rcx
+	.set    ARG2_U8, %dl
+	.set    ARG2_U16, %dx
+	.set    ARG2_U32, %edx
+	.set    ARG2_U64, %rdx
+	.set    ARG3_U32, %r8d
+	.set    ARG3_U64, %r8
+*/
+
+typedef struct {
+    unsigned long P_RAX;
+    unsigned long P_RBX;
+    unsigned long P_RCX;
+    unsigned long P_RDX;
+    unsigned long P_RSI;
+    unsigned long P_RDI;
+    unsigned long P_RFLAGS;
+} SMI_PORT_PARAMS;  
+
+SMI_PORT_PARAMS spp;
+
+typedef struct {
+    unsigned long M_RAX;
+    unsigned long M_RBX;
+    unsigned long M_RCX;
+    unsigned long M_RDX;
+} CPUID_PARAMS;
+
+CPUID_PARAMS cp;
+
+void  hw_lgdt (void *gdtr) {
+
+	asm("lgdt (%0)\n"
+		:
+		:"rcx" (gdtr)
+	  :"rcx"
+	);
+	return;
+}
+
+void hw_sgdt (void * gdtr) {
+//  Store GDTR (to buffer pointed by RCX)
+	asm("sgdt (%0)"
+		:
+		:"rcx" (gdtr)
+		:"rcx"
+	);
+	return;
+}
+
+/*  Read Command Segment Selector
+ *  Stack offsets on entry:
+ *  ax register will contain result
+*/
+int hw_read_cs () {
+		
+	int ret = 0;
+	asm("xor %%rax, %%rax \n\t"
+       "mov %%ax, %%cs \n\t"
+			:"=ax" (ret)
+			::"rax");
+	return ret;
+}
+
+
+//  UINT16 __stdcall
+//  hw_read_ds (
+//          void
+//  );
+//
+//  Read Data Segment Selector
+//
+//  Stack offsets on entry:
+//
+//  ax register will contain result
+//
+void hw_read_ds () {
+	asm("xor %%rax, %%rax \n\t"
+			"mov %%ds, %%ax \n\t"
+			:::
+	);
+	return;
+}
+
+//
+//  void __stdcall
+//  hw_write_ds (
+//          UINT16
+//  );
+//
+//  Write to Data Segment Selector
+/*
+void hw_write_ds(int i) {
+	asm("mov %0, %%ds \n\t"
+			:::
+	);
+	return;
+}
+*/
+
+//
+//  UINT16 __stdcall
+//  hw_read_es (
+//          void
+//  );
+//
+//  Read ES Segment Selector
+//
+//  Stack offsets on entry:
+//
+//  ax register will contain result
+//
+void hw_read_es() {
+
+	asm("xor %%rax, %%rax \n\t"
+			"mov %%es, %%ax \n\t"
+			:::
+	);
+	return;
+}
+
+//
+//  void __stdcall
+//  hw_write_es (
+//          UINT16
+//  );
+//
+//  Write to ES Segment Selector
+//
+/*
+void hw_write_es (short int i) { 
+	asm("mov %%0, %%es"
+			:::
+	);
+	return;
+}
+*/
+//
+//  UINT16 __stdcall
+//  hw_read_ss (
+//          void
+//  );
+//
+//  Read Stack Segment Selector
+//
+//  ax register will contain result
+//
+void hw_read_ss() {
+	asm("xor %%rax, %%rax \n\t"
+      "mov %%es, %%ax \n\t"
+			:::
+	);
+	return;
+}
+
+//
+//  void __stdcall
+//  hw_write_ss (
+//          UINT16
+//  );
+//
+//  Write to Stack Segment Selector
+//
+/*
+void hw_write_ss (short int i) { 
+	asm("mov %%0, %%ss"
+			:::
+	);
+	return;
+}
+*/
+//
+//  UINT16 __stdcall
+//  hw_read_fs (
+//          void
+//  );
+//
+//  Read FS
+//
+//  ax register will contain result
+//
+void hw_read_fs() {
+	asm("xor %%rax, %%rax \n\t"
+      "mov %%fs, %%ax \n\t"
+			:::
+	);
+	return;
+}
+
+//
+//  void __stdcall
+//  hw_write_fs (
+//          UINT16
+//  );
+//
+//  Write to FS
+//
+/*
+void hw_write_fs (short int i) { 
+	asm("mov %%0, %%fs"
+			:::
+	);
+	return;
+}
+*/
+//
+//  UINT16 __stdcall
+//  hw_read_gs (
+//          void
+//  );
+//
+//  Read GS
+//
+//  ax register will contain result
+//
+void hw_read_gs() {
+	asm("xor %%rax, %%rax \n\t"
+      "mov %%gs, %%ax \n\t"
+			:::
+	);
+	return;
+}
+
+//
+//  void __stdcall
+//  hw_write_gs (
+//          UINT16
+//  );
+//
+//  Write to GS
+//
+/*
+void hw_write_gs (short int i) { 
+	asm("mov %%0, %%gs"
+			:::
+	);
+	return;
+}
+*/
+/* 
+ *  UINT64 __stdcall
+ *  hw_read_rsp (void);
+*/
+void hw_read_rsp () {
+		
+		asm("mov %%rsp, %%rax \n\t"
+        "add %%rax, 8 \n\t"
+		:::
+		);
+	return;
+
+}
+void hw_write_to_smi_port() {
+        // save callee saved registers
+	asm("push %%rbp \n\t"
+			"mov %%rbp, %%rsp \n\t" //setup stack frame pointer
+			"push %%rbx \n\t"
+			"push %%rdi \n\t"
+			"push %%rsi \n\t"
+			"push %%r12 \n\t"
+			"push %%r13 \n\t"
+			"push %%r14 \n\t"
+			"push %%r15 \n\t"
+			"lea 16(%%rbp), %%r15 \n\t"//set r15 to point to SMI_PORT_PARAMS struct
+			// normalize stack \n\t"
+			"mov %%rcx, (%%r15) \n\t"
+			"mov %%rdx, 8(%%r15) \n\t"
+			"mov %%r8, 16(%%r15) \n\t"
+			"mov %%r9, 24(%%r15) \n\t"
+			//copy emulator registers into CPU
+			/* RNB: this code can be shortened to just 1 mov for each register
+		   * mov (%%r15), %%rax, mov 8(%%r15), %%rbx, and so on
+			 */
+			"mov (%%r15), %%r8 \n\t"
+			"mov (%%r8), %%rax\n\t"
+			"mov 8(%%r15), %%r8 \n\t"
+			"mov (%%r8), %%rbx \n\t"
+			"mov 16(%%r15), %%r8\n\t"
+			"mov (%%r8), %%rcx \n\t"
+			"mov 24(%%r15), %%r8\n\t"
+			"mov (%%r8), %%rdx\n\t"
+			"mov 32(%%r15), %%r8 \n\t"
+			"mov (%%r8), %%rsi \n\t"
+			"mov 40(%%r15), %%r8 \n\t"
+			"mov (%%r8), %%rdi \n\t"
+			"mov 48(%%r15), %%r8 \n\t"
+			"push (%%r8) \n\t"
+			"popfq \n\t" //rflags = *p_rflags
+
+			//we assume that sp will not change after SMI
+
+			"push %%rbp \n\t"
+			"push %%r15 \n\t"
+//			"out %%dx, %%al \n\t"
+			"out %%al, %%dx \n\t"
+			"pop %%r15 \n\t"
+			"pop %%rbp \n\t"
+			//fill emulator registers from CPU
+			"mov (%%r15), %%r8 \n\t"
+			"mov %%rax, (%%r8) \n\t"
+			"mov 8(%%r15), %%r8 \n\t"
+			"mov %%rbx, (%%r8) \n\t"
+			"mov 16(%%r15), %%r8\n\t"
+			"mov %%rcx, (%%r8) \n\t"
+			"mov 24(%%r15), %%r8 \n\t"
+			"mov %%rdx, (%%r8) \n\t"
+			"mov 32(%%r15), %%r8\n\t"
+			"mov %%rsi, (%%r8) \n\t"
+			"mov 40(%%r15), %%r8 \n\t"
+			"mov %%rdi, (%%r8) \n\t"
+			"mov 48(%%r15), %%r8 \n\t"
+			"pushfq \n\t"
+			"pop (%%r8) \n\t" // *p_rflags = rflags
+			//restore callee saved registers
+			"pop %%r15 \n\t"
+			"pop %%r14 \n\t"
+			"pop %%r13 \n\t"
+			"pop %%r12 \n\t"
+			"pop %%rsi \n\t"
+			"pop %%rdi \n\t"
+			"pop %%rbx \n\t"
+			"pop %%rbp \n\t"
+//			"ret \n\t"
+			:::
+	);
+	return;
+}
+
+/*
+ *  void __stdcall
+ *  hw_cpuid (CPUID_PARAMS *)
+ *
+ *  Execute cpuid instruction
+*/
+void hw_cpuid (CPUID_PARAMS *cp) {
+
+	asm("mov %%rcx, %%r8 \n\t" //RNB: address of struct is assumed to be in %%rcx
+			"mov %%rbx, %%r9 \n\t" //    # save RBX
+        //# fill regs for cpuid
+			"mov (%%r8), %%rax \n\t"
+			"mov 8(%%r8), %%rbx \n\t"
+			"mov 16(%%r8), %%rcx \n\t"
+			"mov 24(%%r8), %%rdx \n\t"
+			"cpuid \n\t"
+			"mov %%rax, (%%r8) \n\t"
+			"mov %%rbx, 8(%%r8) \n\t"
+			"mov %%rcx, 16(%%r8) \n\t"
+			"mov %%rdx, 24(%%r8) \n\t"
+			"mov %%r9, %%rbx \n\t"
+			"mov %%r8, %%rcx \n\t"
+			:::
+	);
+
+	return;
+}

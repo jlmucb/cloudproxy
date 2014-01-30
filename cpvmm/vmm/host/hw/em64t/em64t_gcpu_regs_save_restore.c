f5be8b17e8f95980fff2bb33c1ee1528fa9e0c3f
John Manferdelli <jlmucbmath@gmail.com>
2014-01-30 11:18:27 -0800

    isave register fix

--- cpvmm/vmm/host/hw/em64t/em64t_gcpu_regs_save_restore.c
+++ cpvmm/vmm/host/hw/em64t/em64t_gcpu_regs_save_restore.c
@@ -52,51 +52,47 @@ typedef struct {
 # Assumption - no free registers on entry, all are saved on exit
 #
 */
-void gcpu_save_registers(void) {
-	UINT64 cpuid;
-	UINT64 offset;
-	UINT64 oldrbx = 0, oldrax = 0;
-	UINT64 sizeof_guest_cpu_save_area = sizeof(GUEST_CPU_SAVE_AREA);
+void gcpu_save_registers(void) 
+{
+    UINT64                  cpuid= 0;
+    UINT64                  oldrbx = 0ULL;
+    GUEST_CPU_SAVE_AREA*    save_area= NULL;
 
-	cpuid = hw_cpu_id();
-	offset = cpuid * sizeof_guest_cpu_save_area;
+    cpuid = hw_cpu_id();
+    save_area= g_guest_regs_save_area[cpuid];
 
-		asm volatile (
-			"movq %%rax, %[oldrax] \n\t"
-			"movq	%%rbx, %[oldrbx] \n\t"
-			"movq	%[g_guest_regs_save_area], %%rbx\n\t"
-			"add 	%[offset], %%rbx\n\t"
-			"movq	%%rax, (%%rbx) \n\t"
-			"movq	%[oldrbx], %%rax \n\t"
-			"movq	%%rax, 8(%%rbx) \n\t"
-			"movq %%rcx, 16(%%rbx) \n\t"
-			"movq %%rdx, 24(%%rbx) \n\t"
-			"movq %%rdi, 32(%%rbx) \n\t"
-			"movq %%rsi, 40(%%rbx) \n\t"
-			"movq %%rbp, 48(%%rbx) \n\t"
-			"movq %%r8, 64(%%rbx) \n\t"
-			"movq %%r9, 72(%%rbx) \n\t"
-			"movq %%r10, 80(%%rbx) \n\t"
-			"movq %%r11, 88(%%rbx) \n\t"
-			"movq %%r12, 96(%%rbx) \n\t"
-			"movq %%r13, 104(%%rbx) \n\t"
-			"movq %%r14, 112(%%rbx) \n\t"
-			"movq %%r15, 120(%%rbx) \n\t"
-			//RNB: instead of using 144...182 as offset, it should be 
-			//IA32_REG_GP_COUNT*8
-			"movaps %%xmm0, 144(%%rbx) \n\t"
-			"movaps %%xmm1, 152(%%rbx) \n\t"
-			"movaps %%xmm2, 160(%%rbx) \n\t"
-			"movaps %%xmm3, 168(%%rbx) \n\t"
-			"movaps %%xmm4, 176(%%rbx) \n\t"
-			"movaps %%xmm5, 182(%%rbx) \n\t"
-			"movq %[oldrbx], %%rbx \n\t"
-			"movq %[oldrax], %%rax \n"
-			:[oldrax] "=m" (oldrax), [oldrbx] "=m" (oldrbx)
-			:[cpuid] "m" (cpuid), [g_guest_regs_save_area] "p" (g_guest_regs_save_area),
-			 [offset] "m" (offset)
-			:
-		);
+    asm volatile (
+        "movq   %%rbx, %[oldrbx] \n\t"
+        "movq   %[save_area], %%rbx\n\t"
+        "movq   %%rax, (%%rbx) \n\t"
+        "movq   %[oldrbx], %%rax \n\t"
+        "movq   %%rax, 8(%%rbx) \n\t"
+        "movq   %%rcx, 16(%%rbx) \n\t"
+        "movq   %%rdx, 24(%%rbx) \n\t"
+        "movq   %%rdi, 32(%%rbx) \n\t"
+        "movq   %%rsi, 40(%%rbx) \n\t"
+        "movq   %%rbp, 48(%%rbx) \n\t"
+        "movq   %%r8, 64(%%rbx) \n\t"
+        "movq   %%r9, 72(%%rbx) \n\t"
+        "movq   %%r10, 80(%%rbx) \n\t"
+        "movq   %%r11, 88(%%rbx) \n\t"
+        "movq   %%r12, 96(%%rbx) \n\t"
+        "movq   %%r13, 104(%%rbx) \n\t"
+        "movq   %%r14, 112(%%rbx) \n\t"
+        "movq   %%r15, 120(%%rbx) \n\t"
+        //RNB: used 144...182 as offset instead of IA32_REG_GP_COUNT*8
+        // JLM: how does %rsp get set?
+        "movaps %%xmm0, 144(%%rbx) \n\t"
+        "movaps %%xmm1, 152(%%rbx) \n\t"
+        "movaps %%xmm2, 160(%%rbx) \n\t"
+        "movaps %%xmm3, 168(%%rbx) \n\t"
+        "movaps %%xmm4, 176(%%rbx) \n\t"
+        "movaps %%xmm5, 182(%%rbx) \n\t"
+        "movq   (%%rbx), %%rax \n\t"
+        "movq %[oldrbx], %%rbx \n"
+    : [oldrbx] "=m" (oldrbx)
+    : [cpuid] "m" (cpuid), [save_area] "p" (save_area)
+    :);
 
     return;
 }
@@ -105,48 +101,46 @@ void gcpu_save_registers(void) {
 /*
  * Assumption - all free registers on entry, no free registers on exit
 */
-void gcpu_restore_registers(void) { 
-	UINT64 cpuid;
-	UINT64 offset;
-	UINT64 oldrax = 0;
-	UINT64 sizeof_guest_cpu_save_area = sizeof(GUEST_CPU_SAVE_AREA);
+void gcpu_restore_registers(void) 
+{
+    UINT64                  cpuid= 0;
+    GUEST_CPU_SAVE_AREA*    save_area= NULL;
 
-	cpuid = hw_cpu_id();
-	offset = cpuid * sizeof_guest_cpu_save_area;
+    cpuid = hw_cpu_id();
+    save_area= g_guest_regs_save_area[cpuid];
+
     // restore all XMM first
-	asm(
-			"movq	%[g_guest_regs_save_area], %%rbx\n\t"
-			"addq 	%[offset], %%rbx\n\t"
-			"movaps 144(%%rbx), %%xmm0\n\t"
-			"movaps 152(%%rbx), %%xmm1 \n\t"
-			"movaps 160(%%rbx), %%xmm2 \n\t"
-      "movaps 168(%%rbx), %%xmm3 \n\t"
-      "movaps 176(%%rbx), %%xmm4 \n\t"
-      "movaps 182(%%rbx), %%xmm5 \n\t"
-//      "mov %%rax, (%%rbx) \n\t"
-      // RNB: rbx is restored at the end
-      "movq 16(%%rbx), %%rcx \n\t"
-      "movq 24(%%rbx), %%rdx \n\t"
-      "movq 32(%%rbx), %%rdi \n\t"
-      "movq 40(%%rbx),%%rsi \n\t"
-      "movq 48(%%rbx), %%rbp \n\t"
-      // RNB: rsp is not restored
-      "movq 64(%%rbx), %%r8 \n\t"
-      "movq 72(%%rbx), %%r9 \n\t"
-      "movq 80(%%rbx), %%r10 \n\t"
-      "movq 88(%%rbx), %%r11 \n\t"
-      "movq 96(%%rbx), %%r12 \n\t"
-      "movq 104(%%rbx), %%r13 \n\t"
-      "movq 112(%%rbx), %%r14 \n\t"
-      "movq 120(%%rbx), %%r15 \n\t"
-      "movq (%%rbx), %%rax \n\t"
-      "movq %%rax, %[oldrax] \n\t"
-      "movq 8(%%rbx), %%rax \n"
-      "movq %%rax, %%rbx \n"
-      "movq %[oldrax], %%rax \n"
-			:[oldrax] "+m" (oldrax)
-			:[g_guest_regs_save_area] "p" (g_guest_regs_save_area), [offset] "m" (offset)
-			:
-   );
+    asm(
+        "movq   %[save_area], %%rbx\n\t"
+        "movaps 144(%%rbx), %%xmm0\n\t"
+        "movaps 152(%%rbx), %%xmm1 \n\t"
+        "movaps 160(%%rbx), %%xmm2 \n\t"
+        "movaps 168(%%rbx), %%xmm3 \n\t"
+        "movaps 176(%%rbx), %%xmm4 \n\t"
+        "movaps 182(%%rbx), %%xmm5 \n\t"
+        // RNB: rbx is restored at the end
+        "movq   16(%%rbx), %%rcx \n\t"
+        "movq   24(%%rbx), %%rdx \n\t"
+        "movq   32(%%rbx), %%rdi \n\t"
+        "movq   40(%%rbx),%%rsi \n\t"
+        // RNB: rsp is not restored
+        "movq   64(%%rbx), %%r8 \n\t"
+        "movq   72(%%rbx), %%r9 \n\t"
+        "movq   80(%%rbx), %%r10 \n\t"
+        "movq   88(%%rbx), %%r11 \n\t"
+        "movq   96(%%rbx), %%r12 \n\t"
+        "movq   104(%%rbx), %%r13 \n\t"
+        "movq   112(%%rbx), %%r14 \n\t"
+        "movq   120(%%rbx), %%r15 \n\t"
+        "movq   (%%rbx), %%rax \n\t"
+        "movq   48(%%rbx), %%rbp \n\t"
+        "movq   8(%%rbx), %%rbx \n"
+    : 
+    : [save_area] "p" (save_area)
+    :);
+
    return;
 }
+
+
+

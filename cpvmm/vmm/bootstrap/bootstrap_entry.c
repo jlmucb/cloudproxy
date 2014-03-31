5775296949602d18b458905354567db962854617
John Manferdelli <jlmucbmath@gmail.com>
2014-03-30 19:48:26 -0700

    bug fix

--- cpvmm/vmm/bootstrap/bootstrap_entry.c
+++ cpvmm/vmm/bootstrap/bootstrap_entry.c
@@ -1312,9 +1312,7 @@ int start32_evmm(uint32_t magic, uint32_t initial_entry, multiboot_info_t* mbi)
     // We assume the standard with three modules after bootstrap: 
     //    64-bit evmm, the linux image and initram fs.
     // Everything is decompressed EXCEPT the protected mode portion of linux
-    int l= mbi->mmap_length/sizeof(memory_map_t);
-    bprint("%d modules loaded\n", l);
-    if (l<3) {
+    if (l!=3) {
         bprint("bootstrap error: wrong number of modules %d\n", l);
         LOOP_FOREVER
     }

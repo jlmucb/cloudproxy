8c173350fcefef883533af63d7902ad9177d2545
John Manferdelli <jlmucbmath@gmail.com>
2014-05-13 10:05:46 -0700

    imore explicit initialization parameters

--- cpvmm/vmm/jlmdebug.h
+++ cpvmm/vmm/jlmdebug.h
@@ -10,8 +10,9 @@
 #define VMM_ASSERT(x) \
     if(!(x)) { bprint("VMM_ASSERT\n"); LOOP_FOREVER }
 
-#define LONGLOOP  8000000000ULL
-#define SHORTLOOP 2000000000ULL
+#define LONGLOOP   8000000000ULL
+#define MEDIUMLOOP  800000000ULL
+#define SHORTLOOP   100000000ULL
 
 inline static int evmmdebugwait(UINT64 limit) 
 {

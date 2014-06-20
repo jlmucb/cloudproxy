f471c7a4569ea92dd3ba087a5dee0832921d8069
Tom Roeder <tmroeder@google.com>
2014-06-20 10:30:09 -0700

    Started adding simple RPC calls to the Tao

---
+++ go/src/cloudproxy/tao/tao_server.go
@@ -0,0 +1,22 @@
+package tao
+
+import (
+	"errors"
+)
+
+type TaoServer struct {
+	t Tao
+}
+
+func (ts *TaoServer) GetRandomBytes(r *TaoRPCRequest, s *TaoRPCResponse) error {
+	if r.GetRpc() != TaoRPCOperation_TAO_RPC_GET_RANDOM_BYTES {
+		return errors.New("wrong RPC type")
+	}
+
+	if r.GetSize() <= 0 {
+		return errors.New("Invalid array size")
+	}
+
+	s.Data = make([]byte, r.GetSize())
+	return ts.t.GetRandomBytes(s.GetData())
+}
--- go/src/cloudproxy/tao/taoclient.go
+++ go/src/cloudproxy/tao/taoclient.go
@@ -2,6 +2,8 @@ package tao
 
 import (
 	"net/rpc"
+
+	"code.google.com/p/goprotobuf/proto"
 )
 
 // TaoClient implements the Tao and passes on calls to a parent Tao across an
@@ -11,12 +13,19 @@ type TaoClient struct {
 	HostedProgramHash []byte
 }
 
-// func (t *TaoClient) GetRandomBytes(bytes []byte) (err error) {
-	// err = t.Parent.Call("Tao.GetRandomBytes", bytes)
-	// return
-// }
+func (t *TaoClient) GetRandomBytes(bytes []byte) (err error) {
+	r := &TaoRPCRequest {
+		Rpc: new(TaoRPCOperation),
+		Size: proto.Int32(int32(len(bytes))),
+	}
 
-// func (t *TaoClient) Seal(data []byte) (sealed []byte, err error) {
-	// sealed, err = t.Parent.Call("Tao.Seal", t.HostedProgramHash, data)
-	// return
-// }
+	*r.Rpc = TaoRPCOperation_TAO_RPC_GET_RANDOM_BYTES
+	s := new(TaoRPCResponse)
+	err = t.Parent.Call("TaoServer.GetRandomBytes", r, s)
+	if err != nil {
+		return err
+	}
+
+	copy(bytes, s.Data)
+	return nil
+}

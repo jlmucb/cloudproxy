6bd0827a4259f44e7a4c4b6f0c178419a0f900bf
Kevin Walsh <kwalsh@holycross.edu>
2014-07-14 15:23:25 -0400

    Update bootstrap.sh to use cmake

--- src/build/bootstrap.sh
+++ src/build/bootstrap.sh
@@ -2,10 +2,25 @@
 
 # run this program from fileProxy/src as build/bootstrap.sh
 
+set -e # quit script on first error
+
+build_dir="$(dirname $0)"
+src_dir="${build_dir}/.."
+
 echo Building ninja
-(cd third_party/ninja && ./bootstrap.py)
+(cd ${src_dir}/third_party/ninja && ./bootstrap.py)
 
-echo Configuring ninja build files
-(PATH=${PATH}:`pwd`/third_party/gyp GYP_GENERATORS=ninja gyp build/all.gyp --toplevel-dir=`pwd`)
+echo Configuring ninja build files using cmake for Debug and Release build
+mkdir -p ${src_dir}/out/Debug
+(cd ${src_dir}/out/Debug && cmake -DCMAKE_BUILD_TYPE=Debug -G Ninja ../..)
+mkdir -p ${src_dir}/out/Release
+(cd ${src_dir}/out/Release && cmake -DCMAKE_BUILD_TYPE=Release -G Ninja ../..)
 
-echo To build, execute the command 'third_party/ninja/ninja -C out/Default'
+cat <<END
+To build Tao, add this directory to your PATH:
+   ${src_dir}/third_party/ninja/
+Then, from within ${src_dir}, execute either:
+   ninja -C out/Debug
+Or:
+   ninja -C out/Release
+END

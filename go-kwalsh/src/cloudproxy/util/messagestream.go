b74682d16b239906480d736e3081df6eb34f8ff5
Kevin Walsh <kwalsh@holycross.edu>
2014-07-14 19:40:44 -0400

    go: revise messagestream

--- go-kwalsh/src/cloudproxy/util/messagestream.go
+++ go-kwalsh/src/cloudproxy/util/messagestream.go
@@ -1,26 +1,6 @@
-//  File: messagechannel.go
-//  Author: Kevin Walsh <kwalsh@holycross.edu>
-//
-//  Description: MessageChannel implementation.
-//
-//  Copyright (c) 2013, Google Inc.  All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package tao
+package util
 
 import (
-	"bytes"
 	"code.google.com/p/goprotobuf/proto"
 	"encoding/binary"
 	"errors"
@@ -29,18 +9,41 @@ import (
 	"math"
 	"os"
 )
+// A MessageStream is a bi-direction stream that can read or write three kinds
+// of messages: byte slices, strings, and protobuf messages. It is up to the
+// caller to ensure that each type of write is paired with the appropriate type
+// of read. In addition, a MessageStream can enforce an upper-limit on the size
+// of received messages.
+type MessageStream struct {
+	maxMessageSize uint
+	pipe io.ReadWriteCloser
+}
 
 const (
 	DefaultMaxMessageSize = 20 * 1024 * 1024
 )
 
-type MessageChannel struct {
-	maxMessageSize uint
-	pipe io.ReadWriteCloser
-}
+// type MessageReader interface {
+// 	func MaxMessageSize() uint
+// 	func SetMaxMessageSize(size uint)
+// 	func ReceiveData(bytes []byte) error
+// 	func ReceiveString() (string, error)
+// 	func ReceiveMessage(m proto.Message) error
+// }
+// type MessageWriter interface {
+// 	func SendData(bytes []byte) error
+// 	func SendString(s string) error
+// 	func SendMessage(m proto.Message) error
+// }
+// type MessageReadWritCloser interface {
+// 	io.Closer
+// 	MessageReader
+// 	MessageWriter
+// }
 
-// Close closes a channel. It is safe to call this multiple times.
-func (mc *MessageChannel) Close() error {
+
+// Close closes a MessageStream. It is safe to call this multiple times.
+func (mc *MessageStream) Close() error {
 	if mc.pipe != nil {
 		mc.pipe.Close()
 		mc.pipe = nil
@@ -48,24 +51,19 @@ func (mc *MessageChannel) Close() error {
 	return nil
 }
 
-// IsClosed checks if a channel is closed.
-func (mc *MessageChannel) IsClosed() bool {
-	return mc.pipe == nil
-}
-
 // MaxMessageSize gets the maximum message reception size.
-func (mc *MessageChannel) MaxMessageSize() uint {
+func (mc *MessageStream) MaxMessageSize() uint {
 	return mc.maxMessageSize
 }
 
 // SetMaxMessageSize sets the maximum message reception size.
-func (mc *MessageChannel) SetMaxMessageSize(size uint) {
+func (mc *MessageStream) SetMaxMessageSize(size uint) {
 	mc.maxMessageSize = size
 }
 
 // SendData sends raw data to the channel.
 // Failure will close the channel.
-func (mc *MessageChannel) SendData(bytes []byte) error {
+func (mc *MessageStream) SendData(bytes []byte) error {
 	if mc.pipe == nil {
 		return io.EOF
 	}
@@ -80,7 +78,7 @@ func (mc *MessageChannel) SendData(bytes []byte) error {
 // No maximum message size applies, the caller is expected to supply a
 // reasonable size buffer, which will be filled entirely.
 // Failure or eof will close the channel.
-func (mc *MessageChannel) ReceiveData(bytes []byte) error {
+func (mc *MessageStream) ReceiveData(bytes []byte) error {
 	if mc.pipe == nil {
 		return io.EOF
 	}
@@ -93,7 +91,7 @@ func (mc *MessageChannel) ReceiveData(bytes []byte) error {
 
 // SendString sends a raw string to the channel.
 // Failure will close the channel.
-func (mc *MessageChannel) SendString(s string) error {
+func (mc *MessageStream) SendString(s string) error {
 	n := len(s)
 	if n > math.MaxUint32 {
 		return errors.New("String too large")
@@ -106,7 +104,7 @@ func (mc *MessageChannel) SendString(s string) error {
 
 // ReceiveString receives a string over the channel.
 // Failure or eof will close the channel.
-func (mc *MessageChannel) ReceiveString() (string, error) {
+func (mc *MessageStream) ReceiveString() (string, error) {
 	if mc.pipe == nil {
 		return "", io.EOF
 	}
@@ -117,6 +115,10 @@ func (mc *MessageChannel) ReceiveString() (string, error) {
 		return "", err
 	}
 	n := binary.BigEndian.Uint32(sizebytes[:])
+	if (mc.maxMessageSize > 0 && uint(n) > mc.maxMessageSize) {
+		mc.Close()
+		return "", errors.New("String to large")
+	}
 	strbytes := make([]byte, n)
 	_, err = io.ReadFull(mc.pipe, strbytes)
 	if err != nil {
@@ -128,7 +130,7 @@ func (mc *MessageChannel) ReceiveString() (string, error) {
 
 // SendMessage sends a Message to the channel.
 // Failure will close the channel.
-func (mc *MessageChannel) SendMessage(m proto.Message) error {
+func (mc *MessageStream) SendMessage(m proto.Message) error {
 	bytes, err := proto.Marshal(m)
 	if err != nil {
 		mc.Close()
@@ -139,7 +141,7 @@ func (mc *MessageChannel) SendMessage(m proto.Message) error {
 
 // ReceiveMessage receives a Message (of a particular type) over the
 // channel. Failure or eof will close the channel.
-func (mc *MessageChannel) ReceiveMessage(m proto.Message) error {
+func (mc *MessageStream) ReceiveMessage(m proto.Message) error {
 	s, err := mc.ReceiveString()
 	if err != nil {
 		return err
@@ -152,30 +154,13 @@ func (mc *MessageChannel) ReceiveMessage(m proto.Message) error {
 	return nil
 }
 
-type readerWriterPair struct {
-	io.Reader
-	io.Writer
-	readCloser, writeCloser io.Closer
-}
-
-func (pipe *readerWriterPair) Close() error {
-	var err1, err2 error
-	if pipe.readCloser != nil {
-		err1 = pipe.readCloser.Close()
-	}
-	if pipe.writeCloser != nil {
-		err2 = pipe.writeCloser.Close()
-	}
-	if err1 != nil {
-		return err1
-	} else {
-		return err2
-	}
-}
-
-func DeserializeMessageChannel(s string) *MessageChannel {
+// DeserializeMessageStream takes a string description of the form
+// "tao::FDMessageStream(X, Y)" and returns a MessageStream that uses file
+// descriptor X as the underlying reader and file descriptor write as the
+// underlying writer. 
+func DeserializeMessageStream(s string) *MessageStream {
 	var readfd, writefd uintptr
-	_, err := fmt.Sscanf(s, "tao::FDMessageChannel(%d, %d)", &readfd, &writefd)
+	_, err := fmt.Sscanf(s, "tao::FDMessageStream(%d, %d)", &readfd, &writefd)
 	if err != nil {
 		fmt.Printf("bad scanf: %s\n", s)
 		return nil
@@ -184,14 +169,15 @@ func DeserializeMessageChannel(s string) *MessageChannel {
 	writer := os.NewFile(writefd, "write pipe")
 	var pipe io.ReadWriteCloser
 	if readfd == writefd {
-		pipe = &readerWriterPair{reader, writer, reader, nil}
+		pipe = NewPairReadWriteCloser(reader, writer, nil, writer)
 	} else {
-		pipe = &readerWriterPair{reader, writer, reader, writer}
+		pipe = NewPairReadWriteCloser(reader, writer, reader, writer)
 	}
-	return &MessageChannel{DefaultMaxMessageSize, pipe}
+	return &MessageStream{DefaultMaxMessageSize, pipe}
 }
 
-func NewMessageChannel(pipe io.ReadWriteCloser) *MessageChannel {
-	return &MessageChannel{DefaultMaxMessageSize, pipe}
+// NewMessageStream creates a new MessageStream for the given pipe.
+func NewMessageStream(pipe io.ReadWriteCloser) *MessageStream {
+	return &MessageStream{DefaultMaxMessageSize, pipe}
 }
 

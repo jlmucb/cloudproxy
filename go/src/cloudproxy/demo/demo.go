e45b0bfd3da79d97ae597e4b009367c6c11dafe4
Kevin Walsh <kwalsh@holycross.edu>
2014-07-16 17:57:25 -0400

    todo note

--- go/src/cloudproxy/demo/demo.go
+++ go/src/cloudproxy/demo/demo.go
@@ -1,3 +1,21 @@
+// File: demo.go
+// Author: Kevin Walsh <kwalsh@holycross.edu>
+// Description: Demo of hosted program written in go.
+//
+// Copyright (c) 2013, Google Inc.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -9,6 +27,8 @@ import (
 	"strconv"
 	"strings"
 )
+
+todo: use :=, not var
 
 var server_host = flag.String("host", "localhost", "address for client/server")
 var server_port = flag.Int("port", 8123, "port for client/server")

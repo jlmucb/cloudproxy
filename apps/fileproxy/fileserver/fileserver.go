5411a29e7cfe2ba3dd3dfb337e3cdb882d28fb3a
John Manferdelli <johnmanferdelli@hotmail.com>
2014-10-08 11:29:23 -0700

    moved common code to fileserver

--- apps/fileproxy/fileserver/fileserver.go
+++ apps/fileproxy/fileserver/fileserver.go
@@ -11,340 +11,110 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// FIle: fileserver.go
+//
+// File: fileserver.go
 
 package main
 
 import (
-	"bufio"
-	"crypto/tls"
-	"crypto/x509"
-	"crypto/x509/pkix"
-	"errors"
 	"flag"
 	"fmt"
-	"net"
-	"os"
-	"strings"
-	"github.com/jlmucb/cloudproxy/tao"
+	//"net"
+	//"errors"
+	//"time"
+	//"io/ioutil"
+	//"code.google.com/p/goprotobuf/proto"
+	//"os"
+	//"bufio"
+	//"crypto/tls"
+	//"crypto/x509"
+	// "crypto/x509/pkix"
+	// "crypto/rand"
+	//"net"
+	//"strings"
+
+	tao "github.com/jlmucb/cloudproxy/tao"
 	"github.com/jlmucb/cloudproxy/tao/auth"
-	taonet "github.com/jlmucb/cloudproxy/tao/net"
+	// taonet "github.com/jlmucb/cloudproxy/tao/net"
+	"github.com/jlmucb/cloudproxy/apps/fileproxy"
 )
 
+var hostcfg= flag.String("../hostdomain/tao.config", "../hostdomain/tao.config",  "path to host tao configuration")
 var serverHost = flag.String("host", "localhost", "address for client/server")
 var serverPort = flag.String("port", "8123", "port for client/server")
-var serverAddr string // see main()
-var localMode = flag.Bool("local", true, "Run host demo")
-var clientMode = flag.Bool("client", true, "Run demo client")
-var serverMode = flag.Bool("server", true, "Run demo server")
-var pingCount = flag.Int("n", 5, "Number of client/server pings")
-var demoAuth = flag.String("auth", "tao", "\"tcp\", \"tls\", or \"tao\"")
-var configPath = flag.String("config", "tao.config", "The Tao domain config")
-var ca = flag.String("ca", "", "address for Tao CA, if any")
+var fileserverPath= flag.String("./fileclient_files/", "./fileclient_files/", "fileclient directory")
+var serverAddr string
+var testFile= flag.String("stored_files/originalTestFile", "stored_files/originalTestFile", "test file")
 
-var subprinRule = "(forall P: forall Hash: TrustedProgramHash(Hash) and Subprin(P, %v, Hash) implies MemberProgram(P))"
-var argsRule = "(forall Y: forall P: forall S: MemberProgram(P) and TrustedArgs(S) and Subprin(Y, P, S) implies Authorized(Y, \"Execute\"))"
-var demoRule = "TrustedArgs(ext.Args(%s))"
+var SigningKey tao.Keys
+var SymKeys  []byte
 
-// client/server driver
-
-func doRequest(guard tao.Guard, domain *tao.Domain, keys *tao.Keys) bool {
-	fmt.Printf("client: connecting to %s using %s authentication.\n", serverAddr, *demoAuth)
-	var conn net.Conn
-	var err error
-	network := "tcp"
-
-	switch *demoAuth {
-	case "tcp":
-		conn, err = net.Dial(network, serverAddr)
-	case "tls":
-		conn, err = taonet.DialTLSWithKeys(network, serverAddr, keys)
-	case "tao":
-		conn, err = taonet.DialWithKeys(network, serverAddr, guard, domain.Keys.VerifyingKey, keys)
-	}
-	if err != nil {
-		fmt.Printf("client: error connecting to %s: %s\n", serverAddr, err.Error())
-		return false
-	}
-	defer conn.Close()
-
-	_, err = fmt.Fprintf(conn, "Hello\n")
-	if err != nil {
-		fmt.Printf("client: can't write: %s\n", err.Error())
-		return false
-	}
-	msg, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Printf("client can't read: %s\n", err.Error())
-		return false
-	}
-	msg = strings.TrimSpace(msg)
-	fmt.Printf("client: got reply: %s\n", msg)
-	return true
-}
-
-func newTempCAGuard(v *tao.Verifier) (tao.Guard, error) {
-	g := tao.NewTemporaryDatalogGuard()
-	vprin := v.ToPrincipal()
-	rule := fmt.Sprintf(subprinRule, vprin)
-
-	// Add a rule that says that valid args are the ones we were called with.
-	args := ""
-	for i, a := range os.Args {
-		if i > 0 {
-			args += ", "
-		}
-		args += "\"" + a + "\""
-	}
-	authRule := fmt.Sprintf(demoRule, args)
-
-	if err := g.AddRule(rule); err != nil {
-		return nil, err
-	}
-	if err := g.AddRule(argsRule); err != nil {
-		return nil, err
-	}
-	if err := g.AddRule(authRule); err != nil {
-		return nil, err
-	}
-	return g, nil
-}
-
-func doResponse(conn net.Conn, responseOk chan<- bool) {
-	defer conn.Close()
-
-	// Both the TLS and the Tao/TLS connections and listeners handle
-	// authorization during the Accept operation. So, no extra authorization is
-	// needed here.
-	msg, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Printf("server: can't read: %s\n", err.Error())
-		conn.Close()
-		responseOk <- false
-		return
-	}
-	msg = strings.TrimSpace(msg)
-	fmt.Printf("server: got message: %s\n", msg)
-	responseOk <- true
-	fmt.Fprintf(conn, "echo(%s)\n", msg)
-	conn.Close()
-}
-
-func doServer(stop chan bool, ready, done chan<- bool) {
-	var sock net.Listener
-	var err error
-	var keys *tao.Keys
-	network := "tcp"
-	domain, err := tao.LoadDomain(*configPath, nil)
-	if err != nil {
-		ready <- false
-		done <- true
-		return
-	}
-
-	switch *demoAuth {
-	case "tcp":
-		sock, err = net.Listen(network, serverAddr)
-	case "tls", "tao":
-		keys, err = tao.NewTemporaryTaoDelegatedKeys(tao.Signing, tao.Parent())
-		if err != nil {
-			ready <- false
-			done <- true
-			return
-		}
-		keys.Cert, err = keys.SigningKey.CreateSelfSignedX509(&pkix.Name{
-			Organization: []string{"Google Tao Demo"}})
-		if err != nil {
-			ready <- false
-			done <- true
-			return
-		}
-
-		g := domain.Guard
-		if *ca != "" {
-			na, err := taonet.RequestTruncatedAttestation(network, *ca, keys, domain.Keys.VerifyingKey)
-			if err != nil {
-				ready <- false
-				done <- true
-				return
-			}
-
-			keys.Delegation = na
-			g, err = newTempCAGuard(domain.Keys.VerifyingKey)
-			if err != nil {
-				fmt.Printf("server: couldn't set up a new guard: %s\n", err)
-				return
-			}
-		}
-
-		tlsc, err := taonet.EncodeTLSCert(keys)
-		if err != nil {
-			ready <- false
-			done <- true
-			return
-		}
-		conf := &tls.Config{
-			RootCAs:            x509.NewCertPool(),
-			Certificates:       []tls.Certificate{*tlsc},
-			InsecureSkipVerify: true,
-			ClientAuth:         tls.RequireAnyClientCert,
-		}
-		if *demoAuth == "tao" {
-			sock, err = taonet.Listen(network, serverAddr, conf, g, domain.Keys.VerifyingKey, keys.Delegation)
-		} else {
-			sock, err = tls.Listen(network, serverAddr, conf)
-		}
-	}
-	if err != nil {
-		fmt.Printf("server: can't listen at %s: %s\n", serverAddr, err.Error())
-		ready <- false
-		done <- true
-		return
-	}
-	fmt.Printf("server: listening at %s using %s authentication.\n", serverAddr, *demoAuth)
-	ready <- true
-
-	pings := make(chan bool, 10)
-	connCount := 0
-
-	go func() {
-		for connCount = 0; connCount < *pingCount || *pingCount < 0; connCount++ { // negative means forever
-			conn, err := sock.Accept()
-			if err != nil {
-				fmt.Printf("server: can't accept connection: %s\n", err.Error())
-				stop <- true
-				return
-			}
-			go doResponse(conn, pings)
-		}
-	}()
-
-	pingGood := 0
-	pingFail := 0
-
-loop:
-	for {
-		select {
-		case <-stop:
-			break loop
-		case ok := <-pings:
-			if ok {
-				pingGood++
-			} else {
-				pingFail++
-			}
-		}
-	}
-
-	sock.Close()
-	fmt.Printf("server: handled %d connections, finished %d ok, %d bad pings\n",
-		connCount, pingGood, pingFail)
-
-	done <- true
-}
-
-// Tao Host demo
-
-func hostTaoDemo() error {
-	name, err := tao.Parent().GetTaoName()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("My root name is %s\n", name)
-
-	// TODO(kwalsh) Make a convenience function for this
-	var args []auth.Term
-	for _, arg := range os.Args {
-		args = append(args, auth.Str(arg))
-	}
-	e := auth.PrinExt{Name: "Args", Arg: args}
-	err = tao.Parent().ExtendTaoName(auth.SubPrin{e})
-	if err != nil {
-		return err
-	}
-
-	name, err = tao.Parent().GetTaoName()
-	if err != nil {
-		return err
-	}
-	fmt.Printf("My full name is %s\n", name)
-
-	random, err := tao.Parent().GetRandomBytes(10)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Random bytes  : % x\n", random)
-
-	n, err := tao.Parent().Rand().Read(random)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%d more bytes : % x\n", n, random)
-
-	secret, err := tao.Parent().GetSharedSecret(10, tao.SharedSecretPolicyDefault)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Shared secret : % x\n", secret)
-
-	sealed, err := tao.Parent().Seal(random, tao.SealPolicyDefault)
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Sealed bytes  : % x\n", sealed)
-
-	unsealed, policy, err := tao.Parent().Unseal(sealed)
-	if err != nil {
-		return err
-	}
-	if policy != tao.SealPolicyDefault {
-		return errors.New("unexpected policy on unseal")
-	}
-	fmt.Printf("Unsealed bytes: % x\n", unsealed)
-
-	return nil
-}
 
 func main() {
 	flag.Parse()
 	serverAddr = *serverHost + ":" + *serverPort
-	switch *demoAuth {
-	case "tcp", "tls", "tao":
-	default:
-		fmt.Printf("unrecognized authentication mode: %s\n", *demoAuth)
+
+	hostDomain, err := tao.LoadDomain(*hostcfg, nil)
+	if err != nil {
+		return
+	}
+	fmt.Printf("Domain name: %s\n", hostDomain.ConfigPath)
+
+	e := auth.PrinExt{Name: "fileserver.version.1",}
+	err = tao.Parent().ExtendTaoName(auth.SubPrin{e})
+	if err != nil {
 		return
 	}
 
-	fmt.Printf("Go Tao Demo\n")
-
-	if !tao.Hosted() {
-		fmt.Printf("can't continue: No host Tao available\n")
+	myTaoName, err := tao.Parent().GetTaoName()
+	if(err!=nil) {
 		return
 	}
+	fmt.Printf("fileserver: my name is %s\n", myTaoName)
 
-	if *localMode {
-		err := hostTaoDemo()
+	var derCert []byte
+	sealedSymmetricKey, sealedSigningKey, derCert, delegation, err:= fileproxy.GetMyCryptoMaterial(*fileserverPath) 
+	if(sealedSymmetricKey==nil || sealedSigningKey==nil ||delegation== nil || derCert==nil || err==nil) {
+		fmt.Printf("No key material present\n")
+	}
+
+	// defer zeroBytes(symKeys)
+	if(sealedSymmetricKey!=nil) {
+		SymKeys, policy, err := tao.Parent().Unseal(sealedSymmetricKey)
 		if err != nil {
-			fmt.Printf("error: %s\n", err.Error())
 			return
 		}
+		if policy != tao.SealPolicyDefault {
+			fmt.Printf("fileserver: unexpected policy on unseal\n")
+		}
+		fmt.Printf("Unsealed symKeys: % x\n", SymKeys)
+	} else {
+		SymKeys, err= fileproxy.InitializeSealedSymmetricKeys(*fileserverPath, tao.Parent(), 64)
+		if err != nil {
+			fmt.Printf("fileserver: InitializeSealedSymmetricKeys error: %s\n", err)
+		}
+		fmt.Printf("InitilizedsymKeys: % x\n", SymKeys)
 	}
 
-	serverStop := make(chan bool, 1)
-	serverReady := make(chan bool, 1)
-	serverDone := make(chan bool, 1)
-
-	// TODO(tmroeder): use the Domain and the tao parent to set up the keys and
-	// the guard. Also need to hook the datalog guard into the domain and get
-	// the basic tests working with this guard, especially execution
-	// authorization.
-	go doServer(serverStop, serverReady, serverDone)
-
-	domain, err := tao.LoadDomain(*configPath, nil)
+	// defer zeroBytes(signingKeyBlob)
+	if(sealedSigningKey!=nil) {
+		SigningKey, err:= fileproxy.SigningKeyFromBlob(tao.Parent(), 
+		sealedSigningKey, derCert, delegation)
+		if err != nil {
+			fmt.Printf("fileserver: SigningKeyFromBlob error: %s\n", err)
+		}
+		fmt.Printf("Retrieved Signing key: % x\n", SigningKey)
+	} else {
+		SigningKey, err:=  fileproxy.InitializeSealedSigningKey(*fileserverPath, 
+					tao.Parent(), *hostDomain)
+		if err != nil {
+			fmt.Printf("fileserver: InitializeSealedSigningKey error: %s\n", err)
+		}
+		fmt.Printf("Initilized signingKey: % x\n", SigningKey)
+	}
 	if err != nil {
-		fmt.Printf("error: couldn't load the tao domain from %s\n", *configPath)
-		return
+		fmt.Printf("fileserver: cant get signing key from blob")
 	}
-	<-serverDone
-	fmt.Printf("Done\n")
+	// accept connections
+	fmt.Printf("fileserver: done\n")
 }

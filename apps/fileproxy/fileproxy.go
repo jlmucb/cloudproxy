9dc564a03a7b70acc7a430e9401dca201ddcafca
John Manferdelli <johnmanferdelli@hotmail.com>
2014-10-08 10:40:36 -0700

    fileclient keys

--- apps/fileproxy/fileproxy.go
+++ apps/fileproxy/fileproxy.go
@@ -15,24 +15,20 @@
 package fileproxy
 
 import (
-	//"bufio"
-	//"crypto/tls"
-	//"crypto/x509"
-	//"crypto/x509/pkix"
+	"crypto/x509"
 	"errors"
 	"io/ioutil"
 	"flag"
 	"fmt"
-	// "net"
 	"os"
-	// "strings"
-
+	"code.google.com/p/goprotobuf/proto"
 	"github.com/jlmucb/cloudproxy/tao"
 	"github.com/jlmucb/cloudproxy/tao/auth"
-	//taonet "github.com/jlmucb/cloudproxy/tao/net"
+	taonet "github.com/jlmucb/cloudproxy/tao/net"
 )
 
-var serverAddr string // see main()
+var caAddr = flag.String("ca", "localhost:8124", "The address to listen on")
+var taoChannelAddr = flag.String("ca", "localhost:8124", "The address to listen on")
 var configPath = flag.String("config", "tao.config", "The Tao domain config")
 var ca = flag.String("ca", "", "address for Tao CA, if any")
 var subprinRule = "(forall P: forall Hash: TrustedProgramHash(Hash) and Subprin(P, %v, Hash) implies MemberProgram(P))"
@@ -40,40 +36,44 @@ var argsRule = "(forall Y: forall P: forall S: MemberProgram(P) and TrustedArgs(
 var demoRule = "TrustedArgs(ext.Args(%s))"
 
 // returns sealed symmetric key, sealed signing key, DER encoded cert
-func GetMyCryptoMaterial(path string) ([]byte, []byte,  []byte, error) {
+func GetMyCryptoMaterial(path string) ([]byte, []byte,  []byte, []byte, error) {
 	// stat domain.config
 	fileinfo, err:= os.Stat(path+"sealedsymmetrickey")
 	if(err!=nil) {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	fmt.Printf("Size of %s is %d\n", path+"sealedsymmetrickey", fileinfo.Size())
 	fileinfo, err= os.Stat(path+"sealedsigning")
 	if(err!=nil) {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	fmt.Printf("Size of %s is %d\n", path+"sealedsigningkey", fileinfo.Size())
 	fileinfo, err= os.Stat(path+"cert")
 	if(err!=nil) {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	fmt.Printf("Size of %s is %d\n", path+"cert", fileinfo.Size())
 
 	sealedSymmetricKey, err := ioutil.ReadFile(path+"sealedsymmetrickey")
 	if(err!=nil) {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	sealedSigningKey, err := ioutil.ReadFile(path+"signer")
 	if(err!=nil) {
-		return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
 	derCert, err := ioutil.ReadFile(path+"cert")
 	if(err!=nil) {
-	return nil, nil, nil, err
+		return nil, nil, nil, nil, err
 	}
-	return   sealedSymmetricKey,  sealedSigningKey, derCert, nil
+	ds, err := ioutil.ReadFile(path+"delegation")
+	if ds!=nil || err != nil {
+		return nil, nil, nil, nil, err
+	}
+	return   sealedSymmetricKey, sealedSigningKey, ds, derCert, nil
 }
 
-func MakeSigningKey(t tao.Tao) (*tao.Keys, []byte,  error) {
+func CreateSigningKey(t tao.Tao) (*tao.Keys, []byte,  error) {
 	self, err := t.GetTaoName()
 	k, err:=  tao.NewTemporaryKeys(tao.Signing)
 	if k==nil || k.SigningKey != nil || err!= nil {
@@ -93,21 +93,18 @@ func MakeSigningKey(t tao.Tao) (*tao.Keys, []byte,  error) {
 		Organization: "Google",
 		CommonName: self.String(), }
 	subjectname:= tao.NewX509Name(details)
-	der_cert, err := k.SigningKey.CreateSelfSignedDER(subjectname)
+	derCert, err := k.SigningKey.CreateSelfSignedDER(subjectname)
 	if(err!=nil) {
 		return nil, nil,errors.New("Can't self sign cert\n")
 	}
-	fmt.Printf("der_cert: % x\n", der_cert);
+	fmt.Printf("derCert: % x\n", derCert);
 	fmt.Printf("\n")
-	// cert, err := x509.ParseCertificate(der)
-	signingKeyBlob, err:= tao.MarshalSignerDER(k.SigningKey)
-	// seal and save it
-	// UnmarshalSignerDER(signer []byte) (*Signer, error)
-	// Save DER in Cert
-	if(signingKeyBlob==nil) {
-		return nil, nil, errors.New("Cant produce signing key blob")
+	cert, err := x509.ParseCertificate(derCert)
+	if(err!=nil) {
+		return nil, nil, err
 	}
-	return k, der_cert, nil
+	k.Cert= cert
+	return k, derCert, nil
 }
 
 func InitializeSealedSymmetricKeys(path string, t tao.Tao, keysize int) ([]byte, error) {
@@ -123,9 +120,69 @@ func InitializeSealedSymmetricKeys(path string, t tao.Tao, keysize int) ([]byte,
 	return unsealed, nil
 }
 
-func InitializeSealedSigningKey(path string, t tao.Tao) ([]byte , []byte , error) {
-	// NewTemporaryTaoDelegatedKeys(keyTypes KeyType, t Tao) (*Keys, error)
-	return nil, nil, errors.New("InitializeSealedSymmetricKeys not implemented")
+func InitializeSealedSigningKey(path string, t tao.Tao, domain tao.Domain) (*tao.Keys, error) {
+	k, derCert, err:= CreateSigningKey(t)
+	if (err!=nil || derCert==nil) {
+		fmt.Printf("CreateSigningKey failed\n")
+	}
+	// I need the signed cert as well as the delegation
+	na, err := taonet.RequestTruncatedAttestation("tcp", *ca, k, domain.Keys.VerifyingKey)
+	if(err!=nil || na==nil) {
+		return nil, errors.New("keynegoserver attestation failed")
+	}
+	k.Delegation= na
+	signingKeyBlob, err:= tao.MarshalSignerDER(k.SigningKey)
+	if(err!=nil) {
+		return nil, errors.New("Cant produce signing key blob")
+	}
+	sealedSigningKey, err := t.Seal(signingKeyBlob, tao.SealPolicyDefault)
+	if err != nil {
+		return nil, errors.New("Cant seal signing ken")
+	}
+	err= ioutil.WriteFile(path+"signer", sealedSigningKey, os.ModePerm)
+	if(err!=nil) {
+		return nil, err
+	}
+	k.Cert, err= x509.ParseCertificate(derCert)
+	err= ioutil.WriteFile(path+"cert", derCert, os.ModePerm)
+	if(err!=nil) {
+		return nil, err
+	}
+	delegateBlob, err:= proto.Marshal(k.Delegation)
+	if err != nil {
+		return nil, errors.New("Cant seal random bytes")
+	}
+	err= ioutil.WriteFile(path+"delegation", delegateBlob, os.ModePerm)
+	if(err!=nil) {
+		return nil, err
+	}
+	return k, nil
 }
 
+func SigningKeyFromBlob(t tao.Tao, sealedKeyBlob []byte, delegateBlob []byte, certBlob []byte) (*tao.Keys, error) {
+	k:= &tao.Keys{};
+
+	// k.SetMyKeyPath(path)
+	k.SetKeyType(tao.Signing)
+	cert, err:= x509.ParseCertificate(certBlob)
+	if(err!=nil) {
+		return nil,err
+	}
+	k.Cert= cert
+	k.Delegation = new(tao.Attestation)
+	err= proto.Unmarshal(delegateBlob, k.Delegation)
+	if err != nil {
+		return nil, err
+	}
+	signingKeyBlob, policy, err := tao.Parent().Unseal(sealedKeyBlob)
+	if err != nil {
+		fmt.Printf("fileclient: symkey unsealing error: %s\n")
+	}
+	if policy != tao.SealPolicyDefault {
+		fmt.Printf("fileclient: unexpected policy on unseal\n")
+	}
+	fmt.Printf("Unsealed Signing Key blob: % x\n", signingKeyBlob)
+	k.SigningKey, err= tao.UnmarshalSignerDER(signingKeyBlob)
+	return k, err
+}
 

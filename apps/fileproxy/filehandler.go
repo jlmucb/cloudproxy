dd2d095f91146f4ffed480f2008034e3a9b2ec3a
John Manferdelli <johnmanferdelli@hotmail.com>
2014-10-09 14:05:36 -0700

    more of the resource handler

--- apps/fileproxy/filehandler.go
+++ apps/fileproxy/filehandler.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// File: fileproxy.go
+// File: filehandler.go
 
 package fileproxy
 
@@ -37,26 +37,73 @@ type ResourceInfo struct {
 	resourceName		string
 	resourceType		string
 	resourceLocation	string
-	resourceSize		string
+	resourceSize		int
+	resourceOwner		[]byte   // x509 cert
 	dateCreated		string
 	dateModified		string
-	resourcePointer		string
-	authenticatorType	string
+	authenticatorType	string	 // sha hash usually
 	authenticator		[][]byte
 }
 
 
 type ResourceMaster {
 	program		auth.Prin
-	guard		tao.Guard
+	guard		*tao.Guard
 	baseDirectory	string
 	resourceArray	[]ResourceInfo
 	// Rules
 };
 
-// master structure
-var fileproxy ResourceMaster
+func (m *ResourceMaster) Find(resourcename string) (*ResourceInfo, error){
+	for i:=0; i< m.resourceArray.len();i++ {
+		 if(m.resourceArray[i].resourceName==resourcename) {
+			 return &m.resourceArray[i], nil
+		 }
+	}
+	return nil, nil
+}
 
+func (m *ResourceMaster) Insert(resourcename string) (*ResourceInfo, error){
+	found, err:=  Find(resourcename)
+	return nil, errors.New("Resource exists")
+}
+
+func (m *ResourceMaster) Delete(resourceName string) error {
+	return nil // not implemented
+}
+
+func (m *ResourceMaster) encodeMaster() (string, error){
+}
+
+func (m *ResourceMaster) decodeMaster(record string) error {
+}
+
+func (m *ResourceInfo) encodeResourceInfo() (string, error){
+}
+
+func (m *ResourceInfo) decodeResourceInfo(record string) error {
+}
+
+func (r *ResourceInfo) PrintResourceInfo() {
+	fmt.Printf("Resource name: %s\n", r.resourceName)
+	fmt.Printf("Resource type: %s\n" , r.resourceType)
+	fmt.Printf("Resource location: %s\n" , r.resourceLocation)
+	fmt.Printf("Resource size: %d\n" , r.resourceSize)
+	fmt.Printf("Resource creation date: %s\n" , r.dateCreated)
+	fmt.Printf("Resource modified date: %s\n" , r.dateModified)
+	fmt.Printf("\n")
+}
+
+func (m *ResourceMaster) PrintMaster(bool printResources) {
+	fmt.Printf("Program principal: %s\n", m.program)
+	fmt.Printf("Base Directory: %s\n", m.baseDirectory)
+	fmt.Printf("%d resources\n", m.resourceArray.len())
+	if(printResources) {
+		for i:=0; i< m.resourceArray.len();i++ {
+			 m.resourceArray[i].PrintResourceInfo() 
+		}
+	}
+}
 
 func (r ResourceMaster*)  MarshalResourceMaster() (string, err) {
 }
@@ -68,12 +115,6 @@ func (r ResourceInfo*)  MarshalResourceInfo() (string, err) {
 }
 
 func UnmarshalMarshalResourceInfo(in string) (*ResourceInfo, err) {
-}
-
-func GetResourceData() error {
-}
-
-func SaveResourceData() error {
 }
 
 // Policy
@@ -101,11 +142,104 @@ func SaveResourceData() error {
 //		forall user, delegate-ACTION,resource: Can(user, delegate-ACTION, delegate, resource) --> 
 //			CanAddRule("user says delegate Can(ACTION,user,resource)") and
 //			CanAddRule("user says delegate Can(delegate-ACTION,user,resource)")
-func InitGuard(g *tao.Guard) {
+//
+// It might be cleaner to write if you add some custom predicates in datalog to handle the connection between 
+// ACTION and delegate-ACTION, but I believe it can be made to work without that.
+// the auth language already understands delegation, so you shouldn't need to encode it directly in your rules. 
+// It has a says type and it has a speaksfor.  
+// The "Can" predicate is represented in the Guard terminology by Authorized(name, op, args).
+
+func (m *ResourceMaster) InitGuard(g *tao.Guard, rulefile string) error {
+	//fileGuard := tao.NewTemporaryDatalogGuard()
+	// for now, liberal guard
+	*m.g=  LiberalGuard
+	// no need for rules
+	return nil
 }
 
-func InitResources(handlerFile string, masterInfoFile string,  resourceInfoArrayFile string) error {
+func (m *ResourceMaster) SaveRules(g *tao.Guard, rulefile string) error {
+	// no need for rules
+	return nil
 }
 
-func (h *ResourceMaster) HandleServiceRequest(request string) error {
+func (m *ResourceMaster) GetResourceData(masterInfoFile string,  resourceInfoArrayFile string) error {
+	// read master info
+	// decrypt it
+	// read resourceinfos
+	// decrypt it
+
+	// read rule file
+	// decrypt it
 }
+
+func (m *ResourceMaster) SaveResourceData(masterInfoFile string,  resourceInfoArrayFile string, ruleFile string) error {
+	// encrypt master info
+	// write master info
+	// encrypt fileinfos
+	// write fileinfos
+	// encrypt rules
+	// write rules
+}
+
+// return values: subject, action, resourcename, action-data error
+func decodeRequest(request string) (*string, *string, *string, *string, error) {
+	return nil,nil,nil,nil, errors.New("Cant decode request")
+}
+
+func encodeResponse(status string, message string, size int) {
+}
+
+func sendResponse(conn net.Conn, status string, message string, size int) {
+}
+
+func readRequest(conn net.Conn, resourcename string) {
+}
+
+func writeRequest(conn net.Conn, resourcename string) {
+}
+
+func createRequest(conn net.Conn, subject string, resourcename string) {
+}
+
+func deleteRequest(conn net.Conn, resourcename string) {
+}
+
+func addRuleRequest(conn net.Conn, resourcename string) {
+}
+
+func addOwnerRequest(conn net.Conn, resourcename string) {
+}
+
+func deleteOwnerRequest(conn net.Conn, resourcename string) {
+}
+
+// first return value is terminate flag
+func (m *ResourceMaster) HandleServiceRequest(conn net.Conn, request string) (bool, error) {
+	// decode request
+	subject, action, resourcename, data, err:= decodeRequest(request)
+
+	// is it authorized?
+	var status string
+	var message string
+	ok:= m.guard.IsAuthorized(subject, action, resourcename)
+	if ok == nil {
+		status= "failed"
+		message= "unauthorize"
+		sendErrorResponse(status, message);
+	}
+}
+
+func (m *ResourceMaster) InitMaster(masterInfoDir string, prin tao.Prin)  error {
+	m.GetResourceData(masterInfoDir+"masterinfo",  masterInfoDir+"resources")
+	m.InitGuard(m.guard, masterInfoDir+"rules") {
+}
+
+func (m *ResourceMaster) SaveMaster(masterInfoDir string)  error {
+	err:= m.SaveResourceData(masterInfoDir+"masterinfo",  masterInfoDir+"resources")
+	if err!=nil {
+		fmt.Printf("cant m.SaveResourceData\n")
+		return err
+	}
+	return m.SaveRules(m.guard, masterInfoDir+"rules")
+}
+

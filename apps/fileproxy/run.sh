15a188bd335ad3805d3687afd883ba786b0b202a
Tom Roeder <tmroeder@google.com>
2015-01-09 16:41:17 -0800

    Port fileproxy to use tao_launch.
    This commit ports fileproxy to the new temporary linux_host style that doesn't
    require an install script. Instead, linux_host creates a temporary directory for
    its files and uses that directory for all its work.

--- apps/fileproxy/run.sh
+++ apps/fileproxy/run.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-# This must be run in a directory created by install.sh.
+t=`mktemp /tmp/loc.XXXXXXXX`
+linux_host -tmppath=$t &
+status=$?
+hostpid=$!
+if [ "$status" != "0" ]; then
+	echo "Couldn't start the linux_host in a temporary directory"
+	exit 1
+fi
 
-# Set up the linux host and its environment and get it started.
-scripts/setup.sh
-scripts/start.sh
+echo "Waiting for linux_host to start"
+sleep 2
 
-echo "Waiting for the Linux host to start"
-sleep 5
+# Go to the temp directory set up by linux_host.
+DIR=`cat $t`
+cd ${DIR}
 
 # Set up the key server.
-bin/keynegoserver -password=BogusPass &
+${GOPATH}/bin/keynegoserver -password=BogusPass &
+knpid=$!
+echo "Waiting for the keynegoserver to start"
+sleep 2
 
-# Set up the RollbackMaster and the ResourceMaster under the Tao.
-scripts/host.sh ./bin/rollbackserver -hostconfig=tao.config
-scripts/host.sh ./bin/fileserver -hostconfig=tao.config
+tao_launch -sock ${DIR}/linux_tao_host/admin_socket -- ${GOPATH}/bin/rollbackserver -hostconfig=${DIR}/tao.config -rollbackserver_files=${DIR}/rollbackserver_files
+echo "Waiting for the rollback server to start"
+sleep 2
 
-# Create a user and start the test client.
-bin/tao_admin -newuserkey -common_name="jlm" -pass=BogusPass
-scripts/host.sh ./bin/fileclient -hostconfig=tao.config
+tao_launch -sock ${DIR}/linux_tao_host/admin_socket -- ${GOPATH}/bin/fileserver -hostconfig=${DIR}/tao.config -fileserver_files=${DIR}/fileserver_files
+echo "Waiting for the fileserver to start"
+sleep 2
 
+# Create a user and start the test client
+${GOPATH}/bin/tao_admin -newuserkey -common_name="jlm" -pass=BogusPass
+
+tao_launch -sock ${DIR}/linux_tao_host/admin_socket -- ${GOPATH}/bin/fileclient -hostconfig=${DIR}/tao.config -fileclient_files=${DIR}/fileclient_files -usercreds=${DIR}/usercreds
+
+echo "Waiting for the tests to finish"
+sleep 5
+
+echo "Cleaning up remaining programs"
+pids=`tao_launch -sock ${DIR}/linux_tao_host/admin_socket -operation=list | cut -d' ' -f1 | head -n -1 | sed 's/^pid=//g'`
+for p in $pids; do
+	kill $p
+done
+kill $knpid $hostpid

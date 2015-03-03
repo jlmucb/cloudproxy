60037e081f0b4faeadd982e2501220c15a371e73
Tom Roeder <tmroeder@google.com>
2015-03-03 08:32:15 -0800

    Remove out-of-date run files.
    This commit removes run files that are in old formats or are unnecessary. It
    also adds some shell-script commands to run_processes.sh

--- run_processes.sh
+++ run_processes.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
+# Don't use uninitialized variables
+set -o nounset
+
+# Stop as soon as any command returns an error
+set -o errexit
+
 # Make sure we have sudo privileges before using them to try to start linux_host
 # below.
 sudo test true
 
 TEMP_FILE=`mktemp /tmp/loc.XXXXXXXX`
-echo "About to start linux_host. This requires super-user privileges"
 sudo ${GOPATH}/bin/linux_host -tmppath=$TEMP_FILE &
-status=$?
 HOSTPID=$!
-if [ "$status" != "0" ]; then
-	echo "Couldn't start the linux_host in a temporary directory"
-	exit 1
-fi
 
 echo "Waiting for linux_host to start"
 sleep 5
 
 DIR=`cat $TEMP_FILE`
+
+# Authorize the applications
+${GOPATH}/bin/tao_admin -config_path=${DIR}/tao.config -pass=BogusPass 
+
 DEMO_DIR=$(readlink -e $(dirname $0))
 DSPID=$(${GOPATH}/bin/tao_launch -sock ${DIR}/linux_tao_host/admin_socket ${GOPATH}/bin/demo_server -config=${DIR}/tao.config)
 ${GOPATH}/bin/tao_launch -sock ${DIR}/linux_tao_host/admin_socket ${GOPATH}/bin/demo_client -config=${DIR}/tao.config > /dev/null

d0d714dd3a2484fa98b428321c58bc9a8425a60f
Kevin Walsh <kwalsh@holycross.edu>
2015-08-12 00:09:09 -0400

    Simplify run_processes.sh script
    `tao run` will search path, no need to use gowhich.
    
    `tao run`, demo_client, and demo_server can all use $TAO_DOMAIN, no
    need for command-line args. We still need the arg for sudo commands,
    though, because sudo does not necessarily preserve the env.

--- go/run/scripts/run_processes.sh
+++ go/run/scripts/run_processes.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if [ "$#" == "0" -a "$TAO_DOMAIN" != "" ]; then
-  DOMAIN="$TAO_DOMAIN"
-elif [ "$#" == "1" ]; then
-  DOMAIN="$1"
-else
+if [ "$#" -ge 1 ]; then
+  export TAO_DOMAIN="$1"
+elif [ "$TAO_DOMAIN" == "" ]; then
 	echo "Must supply the path to an initialized domain, or set \$TAO_DOMAIN."
 	exit 1
 fi
@@ -20,18 +18,17 @@ gowhich() {
 TAO="$(gowhich tao)"
 FAKE_PASS=BogusPass
 
-# Make sure we have sudo privileges before using them to try to start linux_host
-# below.
+# Make sure we have sudo privileges before trying to start the tao host
 sudo test true
 
-sudo "$TAO" host start -tao_domain "$DOMAIN" -pass $FAKE_PASS &
+sudo "$TAO" host start -tao_domain "$TAO_DOMAIN" -pass $FAKE_PASS &
 
 echo "Waiting for linux_host to start"
 sleep 5
 
-"$TAO" run -tao_domain "$DOMAIN" "$(gowhich demo_server)" &
+"$TAO" run demo_server &
 sleep 2
-"$TAO" run -tao_domain "$DOMAIN" "$(gowhich demo_client)"
+"$TAO" run demo_client
 
 echo "Shutting down linux_host"
-sudo "$TAO" host stop -tao_domain "$DOMAIN"
+sudo "$TAO" host stop -tao_domain "$TAO_DOMAIN"

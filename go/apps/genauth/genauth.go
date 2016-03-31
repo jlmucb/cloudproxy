8c23c4ca6ec188b7647981f4f4f9628788f3e999
Tom Roeder <tmroeder@google.com>
2016-03-31 12:48:53 -0700

    Add Term/Form decoders and Marshal code

--- go/apps/genauth/genauth.go
+++ go/apps/genauth/genauth.go
@@ -13,8 +13,6 @@ import (
 
 var primitives = map[string]bool{
 	"string": true,
-	"bytes": true,
-	"int": true,
 	"int64": true,
 	"bool": true,
 }
@@ -195,7 +193,7 @@ func writeHeader(constants []Constant, types map[string][]Field, interfaces map[
 		"",
 		"class LogicElement {",
 		" public:",
-		"  virtual bool Marshal(google::protobuf::io::CodedOutputStream* output) = 0;",
+		"  virtual void Marshal(google::protobuf::io::CodedOutputStream* output) = 0;",
 		"};",
 		"",
 		"class Form : public LogicElement {};",
@@ -204,7 +202,7 @@ func writeHeader(constants []Constant, types map[string][]Field, interfaces map[
 	}
 
 	constructor := "  %s(google::protobuf::io::CodedInputStream* input);"
-	marshal := "  bool Marshal(google::protobuf::io::CodedOutputStream* output)"
+	marshal := "  void Marshal(google::protobuf::io::CodedOutputStream* output)"
 
 	header = append(header, "enum class BinaryTags {")
 	for i, constant := range constants {
@@ -268,6 +266,115 @@ func writeHeader(constants []Constant, types map[string][]Field, interfaces map[
 	return header
 }
 
+// The following constants are raw implementation strings that don't need any
+// parametrization.
+const (
+	implHeader = `#include "auth.h"
+
+using google::protobuf::io::CodedInputStream;
+using google::protobuf::io::CodedOutputStream;
+
+namespace {
+	`
+
+	encodeString = `void EncodeString(const string& str, CodedOutputStream* output) {
+  output->WriteVarint32(str.size());
+  output->WriteString(str);
+}
+
+	`
+
+	decodeString = `bool DecodeString(CodedInputStream* input, string* str) {
+  uint32 size = 0;
+  if (!input->ReadVarint32(&size)) return false;
+  return input->ReadString(str, size);
+}
+
+	`
+)
+
+func writeDecoder(constants []Constant, interfaceName string, types map[string]bool) []string {
+	impl := []string{
+		"bool Decode" + interfaceName + "(uint32 tag, CodedInputStream* input, std::unique_ptr<" + interfaceName + ">* value) {",
+		"  switch(tag) {",
+	}
+	for _, constant := range constants {
+		typeName := strings.TrimPrefix(constant.Name, "kTag")
+		if _, ok := types[typeName]; !ok {
+			continue
+		}
+
+		impl = append(impl, []string{
+			"  case " + constant.Name + ": ",
+			"    *value = std::make_unique<" + typeName + ">(input);",
+		}...)
+	}
+
+	impl = append(impl, "  default:", "    return false;")
+	impl = append(impl, "  }", "}", "")
+	return impl
+}
+
+func writeImplementation(constants []Constant, types map[string][]Field, interfaces map[string]bool, formTypes map[string]bool, termTypes map[string]bool) []string {
+	impl := strings.Split(implHeader, "\n")
+	impl = append(impl, strings.Split(encodeString, "\n")...)
+	impl = append(impl, strings.Split(decodeString, "\n")...)
+
+	formDecoder := writeDecoder(constants, "Form", formTypes)
+	termDecoder := writeDecoder(constants, "Term", termTypes)
+
+	impl = append(impl, formDecoder...)
+	impl = append(impl, termDecoder...)
+	impl = append(impl, "}  // namespace", "")
+
+	//constructor := "%s::%s(google::protobuf::io::CodedInputStream* input) {"
+	marshal := "void %s::Marshal(google::protobuf::io::CodedOutputStream* output) {"
+
+	for name, fields := range types {
+		impl = append(impl, fmt.Sprintf(marshal, name))
+		tag := "kTag" + name
+		impl = append(impl, fmt.Sprintf("  output->WriteVarint32(%s);", tag))
+		for _, field := range fields {
+			typeName := field.TypeName
+
+			// This code currently only supports string and *int64 as primitives.
+			if typeName == "string" {
+				impl = append(impl, fmt.Sprintf("  EncodeString(%s_, output);", field.Name))
+				continue
+			}
+
+			if typeName == "int64" && field.Type == StarType {
+				// This has a boolean value that says whether or
+				// not to expect an int64 field next.
+				impl = append(impl, []string{
+					fmt.Sprintf("  int value = %s_ == nullptr ? 0 : 1;", field.Name),
+					"  output->WriteVarint32(value);",
+					fmt.Sprintf("  if (%s_ == nullptr) {", field.Name),
+					fmt.Sprintf("    output->WriteVarint64(%s_);", field.Name),
+					"  }",
+				}...)
+				continue
+			}
+
+			switch field.Type {
+			case IdentType:
+				impl = append(impl, fmt.Sprintf("  %s_->Marshal(output);", field.Name))
+			case ArrayType:
+				impl = append(impl, []string{
+					fmt.Sprintf("  output->WriteVarint32(%s_.size());", field.Name),
+					fmt.Sprintf("  for(auto& elt : %ss_) {", field.Name),
+					"    elt->Marshal(input);",
+					"  }",
+				}...)
+			}
+		}
+
+		impl = append(impl, "}", "")
+	}
+
+	return impl
+}
+
 func main() {
 	output := os.Stdout
 
@@ -322,6 +429,11 @@ func main() {
 
 	header := writeHeader(constantVisitor.Constants, tv.ConcreteTypes, tv.InterfaceTypes, formWalker.types, termWalker.types)
 	for _, line := range header {
+		fmt.Fprintln(output, line)
+	}
+
+	impl := writeImplementation(constantVisitor.Constants, tv.ConcreteTypes, tv.InterfaceTypes, formWalker.types, termWalker.types)
+	for _, line := range impl {
 		fmt.Fprintln(output, line)
 	}
 

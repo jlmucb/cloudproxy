559f0a1c1c76219a5e7164967d1cce4e60ae60fb
sid <sidtelang@google.com>
2016-07-07 14:16:30 -0700

    Quote server working

--- go/tpm2/apps/Endorsement/MakeEndorsementCert.go
+++ go/tpm2/apps/Endorsement/MakeEndorsementCert.go
@@ -16,27 +16,33 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"time"
 
+	"github.com/jlmucb/cloudproxy/go/tao"
 	"github.com/jlmucb/cloudproxy/go/tpm2"
 )
 
 // This program makes the endorsement certificate given the Policy key.
 func main() {
-	keySize := flag.Int("modulus size",  2048, "Modulus size for keys")
-	keyName := flag.String("Endorsement key name",
+	keySize := flag.Int("modulus_size", 2048, "Modulus size for keys")
+	keyName := flag.String("endorsement_key_name",
 		"JohnsHw", "endorsement key name")
-	endorsementCertFile := flag.String("Endorsement save file",
+	endorsementCertFile := flag.String("endorsement_save_file",
 		"endorsement.cert.der", "endorsement save file")
-	policyCertFile := flag.String("Policy cert file",
+	policyCertFile := flag.String("policy_cert_file",
 		"policy.cert.go.der", "cert file")
-	policyKeyFile:= flag.String("Policy key file",  "policy.go.bin",
+	policyKeyFile := flag.String("policy_key_file", "policy.go.bin",
 		"policy save file")
-	policyKeyPassword := flag.String("Policy key password",  "xxzzy",
+	policyKeyPassword := flag.String("policy_key_password", "xxzzy",
 		"policy key password")
+	policyKeyIsEcdsa := flag.Bool("policy_key_is_ecdsa", false, "Whether the policy key is a ECDSA key")
+	policyKeyDir := flag.String("policy_key_dir", "./keys/", "Path to policy keys")
 	flag.Parse()
 	fmt.Printf("Policy key password: %s\n", *policyKeyPassword)
 
@@ -52,7 +58,7 @@ func main() {
 	defer rw.Close()
 
 	// Flushall
-	err =  tpm2.Flushall(rw)
+	err = tpm2.Flushall(rw)
 	if err != nil {
 		fmt.Printf("Flushall failed\n")
 		return
@@ -60,25 +66,8 @@ func main() {
 
 	var notBefore time.Time
 	notBefore = time.Now()
-	validFor := 365*24*time.Hour
+	validFor := 365 * 24 * time.Hour
 	notAfter := notBefore.Add(validFor)
-
-	serializePolicyKey, err := ioutil.ReadFile(*policyKeyFile)
-	if err != nil {
-		fmt.Printf("Can't get serialized policy key\n")
-		return
-	}
-	derPolicyCert, err := ioutil.ReadFile(*policyCertFile)
-	if err != nil {
-		fmt.Printf("Can't get policy cert %s\n", *policyCertFile)
-		return
-	}
-
-	policyKey, err := tpm2.DeserializeRsaKey(serializePolicyKey)
-	if err != nil {
-		fmt.Printf("Can't get deserialize policy key\n")
-		return
-	}
 
 	ekHandle, _, err := tpm2.CreateEndorsement(rw, uint16(*keySize), pcrs)
 	if err != nil {
@@ -86,11 +75,71 @@ func main() {
 		return
 	}
 	defer tpm2.FlushContext(rw, ekHandle)
-	endorsementCert, err := tpm2.GenerateHWCert(rw, ekHandle,
-		*keyName, notBefore, notAfter, tpm2.GetSerialNumber(),
-		derPolicyCert, policyKey)
-	if err != nil {
-		fmt.Printf("Can't create endorsement cert\n")
+
+	var endorsementCert []byte
+	if *policyKeyIsEcdsa {
+		// Load keys from policyKeyDir if keys are present there.
+		policyKey, err := tao.NewOnDiskPBEKeys(tao.Signing, []byte(*policyKeyPassword), *policyKeyDir, nil)
+		if err != nil {
+			fmt.Println("Error in getting policy cert: ", err)
+			return
+		}
+		if policyKey.Cert == nil {
+			fmt.Println("Missing cert in policy key.")
+			return
+		}
+		hwPublic, err := tpm2.GetRsaKeyFromHandle(rw, ekHandle)
+		if err != nil {
+			fmt.Println("Can't get endorsement public key: ", err)
+			return
+		}
+		// TODO(sidtelang): move this to tpm2/support.go
+		serialNumber := tpm2.GetSerialNumber()
+		fmt.Printf("Serial: %x\n", serialNumber)
+		fmt.Printf("notBefore: %s, notAfter: %s\n", notBefore, notAfter)
+		signTemplate := x509.Certificate{
+			SerialNumber: serialNumber,
+			Subject: pkix.Name{
+				Organization: []string{*keyName},
+				CommonName:   *keyName,
+			},
+			NotBefore:             notBefore,
+			NotAfter:              notAfter,
+			KeyUsage:              x509.KeyUsageCertSign,
+			ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+			BasicConstraintsValid: true,
+			// IsCA: false,
+			IsCA: true,
+		}
+		endorsementCert, err = x509.CreateCertificate(rand.Reader, &signTemplate, policyKey.Cert,
+			hwPublic, policyKey.SigningKey.GetSigner())
+		if err != nil {
+			fmt.Println("Can't create endorsement certificate: ", err)
+			return
+		}
+	} else {
+		serializePolicyKey, err := ioutil.ReadFile(*policyKeyFile)
+		if err != nil {
+			fmt.Printf("Can't get serialized policy key\n")
+			return
+		}
+		derPolicyCert, err := ioutil.ReadFile(*policyCertFile)
+		if err != nil {
+			fmt.Printf("Can't get policy cert %s\n", *policyCertFile)
+			return
+		}
+
+		policyKey, err := tpm2.DeserializeRsaKey(serializePolicyKey)
+		if err != nil {
+			fmt.Printf("Can't get deserialize policy key\n")
+			return
+		}
+		endorsementCert, err = tpm2.GenerateHWCert(rw, ekHandle,
+			*keyName, notBefore, notAfter, tpm2.GetSerialNumber(),
+			derPolicyCert, policyKey)
+		if err != nil {
+			fmt.Printf("Can't create endorsement cert\n")
+		}
 	}
 	fmt.Printf("Endorsement cert: %x\n", endorsementCert)
 	ioutil.WriteFile(*endorsementCertFile, endorsementCert, 0644)

7537718fb3f01381a83c0ecc194ed9f2ebf852fc
John Manferdelli <johnmanferdelli@hotmail.com>
2017-06-22 18:58:31 -0700

    ciphersuite

--- go/tao/tao.go
+++ go/tao/tao.go
@@ -113,8 +113,8 @@ type Tao interface {
 //	of SHA-384 and P-384 make no sense to me since SHA-512 should be equivalent to
 //	AES-256 in security.  Oh well.
 const (
-	Basic128BitCipherSuite = "AES-128-CTR-ECC-P256-SHA-256-HMAC-SHA-256"
-	Basic256BitCipherSuite = "AES-256-CTR-ECC-P384-SHA-384-HMAC-SHA-384"
+	Basic128BitCipherSuite = "sign:ecdsap256,crypt:aes128-ctr-hmacsha256,derive:hdkf-sha256"
+	Basic256BitCipherSuite = "sign:ecdsap384,crypt:aes256-ctr-hmacsha384,derive:hdkf-sha256"
 )
 // The following variable, defined in "tao_cipher_suite.go," selects the cipher suite.
 // var TaoCryptoSuite = Basic128BitCipherSuite

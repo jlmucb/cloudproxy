9de6e97682379390d03d7a8bba4e26b3616ea17b
John Manferdelli <johnmanferdelli@hotmail.com>
2017-07-09 11:59:13 -0700

    emove old code

--- go/support_libraries/tao_support/taosupport.pb.go
+++ go/support_libraries/tao_support/taosupport.pb.go
@@ -26,7 +26,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+// const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
 type SavedProgramData struct {
 	FilePath         *string  `protobuf:"bytes,1,opt,name=file_path,json=filePath" json:"file_path,omitempty"`
@@ -105,7 +105,7 @@ func init() {
 	proto.RegisterType((*SavedProgramData)(nil), "tao_support.SavedProgramData")
 }
 
-func init() { proto.RegisterFile("taosupport.proto", fileDescriptor0) }
+// func init() { proto.RegisterFile("taosupport.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
 	// 247 bytes of a gzipped FileDescriptorProto
